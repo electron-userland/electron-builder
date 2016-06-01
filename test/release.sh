96a00e70c37ecc391513aa2fe79746ae1b936d7b
develar <develar@gmail.com>
2016-06-01 19:13:30 +0200

    test: don't split OS X tests for now

--- test/release.sh
+++ test/release.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-if [ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" && "$AUTO_PUBLISH" != "false" ]
-then
-  npm run semantic-release
-fi
+if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" && "$AUTO_PUBLISH" != "false" ]]; then npm run semantic-release ; fi
