078412a59051e94cdb02258324c6736ae6b98bbf
develar <develar@gmail.com>
2016-10-26 09:07:13 +0200

    test: 6 and 7 nodejs versions

--- docker/build.sh
+++ docker/build.sh
@@ -3,7 +3,7 @@ set -e
 
 docker build -t electronuserland/electron-builder:base docker/base
 
-docker build -t electronuserland/electron-builder:6 -t electronuserland/electron-builder:latest docker/6
-docker build -t electronuserland/electron-builder:4 docker/4
+docker build -t electronuserland/electron-builder:7 -t electronuserland/electron-builder:latest docker/7
+docker build -t electronuserland/electron-builder:6 docker/6
 
 docker build -t electronuserland/electron-builder:wine docker/wine
