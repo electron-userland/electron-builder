6597810756ff083e0a6a045c61a4c11e865e933f
develar <develar@gmail.com>
2017-02-08 09:37:43 +0100

    chore: add new packages to npm-publish.sh

--- packages/npm-publish.sh
+++ packages/npm-publish.sh
@@ -6,6 +6,8 @@ ln -f README.md packages/electron-builder/README.md
 npm publish packages/electron-builder-http || true
 npm publish packages/electron-builder-core || true
 npm publish packages/electron-builder-util || true
+npm publish packages/electron-builder-publisher || true
+npm publish packages/electron-publisher-s3 || true
 npm publish packages/electron-builder || true
 npm publish packages/electron-builder-squirrel-windows || true
 npm publish packages/electron-updater || true
