e048509ac61d4fec975f935586ab5a3d5088aea7
Carlos Fernández <sirikon2@gmail.com>
2017-10-05 18:16:54 +0200

    Use BUILD_BUILDNUMBER env var as buildNumber for VSTS CI
    In VSTS Builds, the build variable containing the build number is `BUILD_BUILDNUMBER`
    
    https://docs.microsoft.com/en-us/vsts/build-release/concepts/definitions/build/variables

--- packages/electron-builder/src/appInfo.ts
+++ packages/electron-builder/src/appInfo.ts
@@ -20,7 +20,7 @@ export class AppInfo {
       buildVersion = info.config.buildVersion
     }
 
-    this.buildNumber = process.env.BUILD_NUMBER || process.env.TRAVIS_BUILD_NUMBER || process.env.APPVEYOR_BUILD_NUMBER || process.env.CIRCLE_BUILD_NUM
+    this.buildNumber = process.env.BUILD_NUMBER || process.env.TRAVIS_BUILD_NUMBER || process.env.APPVEYOR_BUILD_NUMBER || process.env.CIRCLE_BUILD_NUM || process.env.BUILD_BUILDNUMBER
     if (buildVersion == null) {
       buildVersion = this.version
       if (!isEmptyOrSpaces(this.buildNumber)) {
