e4a2ede74af46ad131526eb74c9cd418b7a54723
develar <develar@gmail.com>
2017-12-29 17:45:21 +0100

    feat: use NodeJS 9 in the docker images

--- docker/build.sh
+++ docker/build.sh
@@ -3,8 +3,7 @@ set -e
 
 docker build -t electronuserland/builder:base docker/base
 
-docker build -t electronuserland/builder:8 -t electronuserland/builder:latest docker/8
-docker build -t electronuserland/builder:6 docker/6
+docker build -t electronuserland/builder:9 -t electronuserland/builder:latest docker/9
 
 docker build -t electronuserland/builder:wine docker/wine
 docker build -t electronuserland/builder:wine-mono docker/wine-mono
