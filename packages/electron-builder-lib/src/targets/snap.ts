78f2919868de453ac3e4a95641ea9ac5448c5712
Martin Wimpress <code@flexion.org>
2018-01-04 10:44:50 +0100

    feat(snap): update the default stage-packages and change the default desktop helper to desktop-gtk2
    Close #2432

--- packages/electron-builder-lib/src/targets/snap.ts
+++ packages/electron-builder-lib/src/targets/snap.ts
@@ -77,8 +77,8 @@ export default class SnapTarget extends Target {
 
     const isUseDocker = process.platform !== "linux"
     // libxss1, libasound2, gconf2 - was "error while loading shared libraries: libXss.so.1" on Xubuntu 16.04
-    const defaultStagePackages = ["libnotify4", "libappindicator1", "libxtst6", "libnss3", "libxss1", "fontconfig-config", "gconf2", "libasound2", "pulseaudio"]
-    const defaultAfter = ["desktop-glib-only"]
+    const defaultStagePackages = ["libasound2", "libgconf2-4", "libnotify4", "libnspr4", "libnss3", "libpcre3", "libpulse0", "libxss1", "libxtst6"]
+    const defaultAfter = ["desktop-gtk2"]
     const after = replaceDefault(options.after, defaultAfter)
     snap.parts = {
       app: {
