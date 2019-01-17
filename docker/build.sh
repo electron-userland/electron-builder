f10ffc08013d5c517b3294b030fa27961d5e506e
Rakshith Ravi <rakshithravi1997@gmail.com>
2019-01-17 11:34:32 +0530

    feat: Test add node v11 support

--- docker/build.sh
+++ docker/build.sh
@@ -3,7 +3,7 @@ set -e
 
 docker build -t electronuserland/builder:base -t electronuserland/builder:base-05.18 docker/base
 
-docker build -t electronuserland/builder:10 -t electronuserland/builder:latest -t electronuserland/builder:10-05.18 docker/10
+docker build -t electronuserland/builder:11 -t electronuserland/builder:latest -t electronuserland/builder:11-05.18 docker/11
 
 docker build -t electronuserland/builder:wine docker/wine
 docker build -t electronuserland/builder:wine-mono docker/wine-mono
