c7410c980e55f680920aadc8fb8d7dffe16edd18
Dan Graham <dan@saucelabs.com>
2019-03-08 11:32:10 -0800

    fix: Increase log buffer for 7zip exec
    (addresses https://github.com/electron-userland/electron-builder/issues/3704)

--- packages/builder-util/src/util.ts
+++ packages/builder-util/src/util.ts
@@ -89,7 +89,7 @@ export function exec(file: string, args?: Array<string> | null, options?: ExecFi
   return new Promise<string>((resolve, reject) => {
     execFile(file, args, {
     ...options,
-    maxBuffer: 10 * 1024 * 1024,
+    maxBuffer: 1000 * 1024 * 1024,
     env: getProcessEnv(options == null ? null : options.env),
   }, (error, stdout, stderr) => {
       if (error == null) {
