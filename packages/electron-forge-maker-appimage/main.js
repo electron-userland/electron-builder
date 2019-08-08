14c9027a38b52c17b36cc0f08cf14a3006821c6c
Marcus10110 <markgarrison@saleae.com>
2019-08-07 18:56:25 -0700

    Updated electron-forge-maker-appimage to use a Class, as required by electron-forge/packages/api/core/src/api/make.ts addresses electron-forge issue 26 https://github.com/electron-userland/electron-forge/issues/26

--- packages/electron-forge-maker-appimage/main.js
+++ packages/electron-forge-maker-appimage/main.js
@@ -1,13 +1,37 @@
-"use strict"
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-})
+Object.defineProperty(exports, '__esModule', {
+  value: true,
+});
 
-const buildForge = require("app-builder-lib").buildForge
+const buildForge = require('app-builder-lib').buildForge;
+const Target = require('app-builder-lib').Target;
+const platform = require('os').platform;
 
-exports.isSupportedOnCurrentPlatform = () => Promise.resolve(true)
+class MakerAppImage extends Target {
+  constructor(configFetcher, providedPlatforms) {
+    super(configFetcher, providedPlatforms);
+    this.name = 'AppImage';
+    this.defaultPlatforms = ['linux'];
+  }
+  isSupportedOnCurrentPlatform() {
+    return platform() === 'linux';
+  }
+  make(options) {
+    return buildForge(options, { linux: [`appimage:${options.targetArch}`] });
+  }
 
-exports.default = function (options) {
-  return buildForge(options, {linux: [`appimage:${options.targetArch}`]})
+  get platforms() {
+    if (this.providedPlatforms) return this.providedPlatforms;
+    return this.defaultPlatforms;
+  }
+
+  prepareConfig(targetArch) {
+    if (typeof this.configFetcher === 'function') {
+      this.config = this.configFetcher(targetArch);
+    } else {
+      this.config = this.configFetcher;
+    }
+  }
 }
+exports.default = MakerAppImage;
