d4336ff8081e9a6fb1b79203f49d7135ef74e7d7
dangeredwolf <dangeredwolf@gmail.com>
2019-09-11 15:41:46 -0400

    Bump testing electron to 6.0.8

--- test/src/helpers/testConfig.ts
+++ test/src/helpers/testConfig.ts
@@ -1,7 +1,7 @@
 import * as os from "os"
 import * as path from "path"
 
-export const ELECTRON_VERSION = "6.0.0"
+export const ELECTRON_VERSION = "6.0.8"
 
 export function getElectronCacheDir() {
   if (process.platform === "win32") {
