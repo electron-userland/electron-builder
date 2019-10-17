0be96c63c9715095afdef30105d95dfe6453b072
jtendik <jtendik@igloosoftware.com>
2019-10-17 14:11:53 -0400

    added keychain flag to getProvisioningProfileAsync

--- packages/app-builder-lib/electron-osx-sign/util-provisioning-profiles.js
+++ packages/app-builder-lib/electron-osx-sign/util-provisioning-profiles.js
@@ -55,14 +55,21 @@ Object.defineProperty(ProvisioningProfile.prototype, 'type', {
  * Returns a promise resolving to a ProvisioningProfile instance based on file.
  * @function
  * @param {string} filePath - Path to provisioning profile.
+ * @param {string} keychain - Keychain to use when unlocking provisioning profile.
  * @returns {Promise} Promise.
  */
-var getProvisioningProfileAsync = module.exports.getProvisioningProfileAsync = function (filePath) {
-  return execFileAsync('security', [
+var getProvisioningProfileAsync = module.exports.getProvisioningProfileAsync = function (filePath, keychain = null) {
+  var securityArgs = [
     'cms',
     '-D', // Decode a CMS message
     '-i', filePath // Use infile as source of data
-  ])
+  ];
+
+  if (keychain) {
+    securityArgs.push('-k', keychain);
+  }
+
+  return execFileAsync('security', securityArgs)
     .then(async function (result) {
       // todo read directly
       const tempFile = path.join(os.tmpdir(), `${require('crypto').createHash('sha1').update(filePath).digest("hex")}.plist`)
@@ -150,7 +157,7 @@ module.exports.preEmbedProvisioningProfile = function (opts) {
     if (opts['provisioning-profile'] instanceof ProvisioningProfile) {
       return embedProvisioningProfile()
     } else {
-      return getProvisioningProfileAsync(opts['provisioning-profile'])
+      return getProvisioningProfileAsync(opts['provisioning-profile'], opts['keychain'])
         .then(function (provisioningProfile) {
           opts['provisioning-profile'] = provisioningProfile
         })
