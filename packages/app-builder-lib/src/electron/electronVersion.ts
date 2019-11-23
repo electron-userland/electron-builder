57c00b669f4270c697ebb29a1bc98f7420126b5d
Shadix Aced <30985701+ShadixAced@users.noreply.github.com>
2019-11-24 00:25:20 +0100

    Fixing for test

--- packages/app-builder-lib/src/electron/electronVersion.ts
+++ packages/app-builder-lib/src/electron/electronVersion.ts
@@ -59,7 +59,7 @@ export async function computeElectronVersion(projectDir: string, projectMetadata
 
   const electronVersionFromMetadata = findFromPackageMetadata(await projectMetadata!!.value)
   const electronPackageName = findFromPackageMetadata(await projectMetadata!!.value)
-  
+
   if (electronPackageName === "electron-nightly") {
     log.warn("You are using a nightly version of electron, be warned that those builds are highly unstable.")
     try {
