244abfbaaed2999bdf2c7ae688f7db99c1cba2c2
develar <develar@gmail.com>
2020-01-21 08:23:18 +0100

    feat: support resolving of `latest` version for electron-nightly

--- packages/app-builder-lib/src/electron/electronVersion.ts
+++ packages/app-builder-lib/src/electron/electronVersion.ts
@@ -1,4 +1,5 @@
 import { InvalidConfigurationError, log } from "builder-util"
+import { parseXml } from "builder-util-runtime"
 import { httpExecutor } from "builder-util/out/nodeHttpExecutor"
 import { readJson } from "fs-extra"
 import { Lazy } from "lazy-val"
@@ -57,32 +58,27 @@ export async function computeElectronVersion(projectDir: string, projectMetadata
     return result
   }
 
-  const electronVersionFromMetadata = findFromPackageMetadata(await projectMetadata!!.value)
-  const electronPackageName = findFromPackageMetadata(await projectMetadata!!.value)
-
-  if (electronPackageName === "electron-nightly") {
-    log.warn("You are using a nightly version of electron, be warned that those builds are highly unstable.")
-    try {
-      const releaseInfo = JSON.parse((await httpExecutor.request({
-          hostname: "github.com",
-          path: "/electron/nightlies/releases/latest",
-          headers: {
-            accept: "application/json",
-          },
-      }))!!)
-      return (releaseInfo.tag_name.startsWith("v")) ? releaseInfo.tag_name.substring(1) : releaseInfo.tag_name
-    }
-    catch (e) {
-      log.warn(e)
-    }
-    throw new InvalidConfigurationError(`Cannot find electron in '${path.join(projectDir, "package.json")}'`)
+  const dependency = findFromPackageMetadata(await projectMetadata!!.value)
+  if (dependency?.name === "electron-nightly") {
+    log.info("You are using a nightly version of electron, be warned that those builds are highly unstable.")
+    const feedXml = await httpExecutor.request({
+      hostname: "github.com",
+      path: `/electron/nightlies/releases.atom`,
+      headers: {
+        accept: "application/xml, application/atom+xml, text/xml, */*",
+      },
+    })
+    const feed = parseXml(feedXml!!)
+    const latestRelease = feed.element("entry", false, `No published versions on GitHub`)
+    const v = latestRelease.element("link").attribute("href").match(/\/tag\/v?([^\/]+)$/)!![1]
+    return v.startsWith("v") ? v.substring(1) : v
   }
-  else if (electronPackageName === "electron" || electronPackageName === "electron-prebuilt" || electronPackageName === "electron-prebuilt-compile" && electronVersionFromMetadata === "latest") {
+  else if (dependency?.version === "latest") {
     log.warn("Electron version is set to \"latest\", but it is recommended to set it to some more restricted version range.")
     try {
       const releaseInfo = JSON.parse((await httpExecutor.request({
         hostname: "github.com",
-        path: "/electron/electron/releases/latest",
+        path: `/electron/${dependency?.name === "electron-nightly" ? "nightlies" : "electron"}/releases/latest`,
         headers: {
           accept: "application/json",
         },
@@ -96,15 +92,21 @@ export async function computeElectronVersion(projectDir: string, projectMetadata
     throw new InvalidConfigurationError(`Cannot find electron dependency to get electron version in the '${path.join(projectDir, "package.json")}'`)
   }
 
-  if (electronVersionFromMetadata == null || !/^\d/.test(electronVersionFromMetadata)) {
-    const versionMessage = electronVersionFromMetadata == null ? "" : ` and version ("${electronVersionFromMetadata}") is not fixed in project`
+  const version = dependency?.version
+  if (version == null || !/^\d/.test(version)) {
+    const versionMessage = version == null ? "" : ` and version ("${version}") is not fixed in project`
     throw new InvalidConfigurationError(`Cannot compute electron version from installed node modules - none of the possible electron modules are installed${versionMessage}.\nSee https://github.com/electron-userland/electron-builder/issues/3984#issuecomment-504968246`)
   }
 
-  return semver.coerce(electronVersionFromMetadata)!!.toString()
+  return semver.coerce(version)!!.toString()
 }
 
-function findFromPackageMetadata(packageData: any): string | null {
+interface NameAndVersion {
+  readonly name: string
+  readonly version: string
+}
+
+function findFromPackageMetadata(packageData: any): NameAndVersion | null {
   for (const name of electronPackages) {
     const devDependencies = packageData.devDependencies
     let dep = devDependencies == null ? null : devDependencies[name]
@@ -113,7 +115,7 @@ function findFromPackageMetadata(packageData: any): string | null {
       dep = dependencies == null ? null : dependencies[name]
     }
     if (dep != null) {
-      return dep
+      return {name, version: dep}
     }
   }
   return null
