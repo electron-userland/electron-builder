11aa06c701a2ff091139a8bf8277151f7172efbb
Jaimy de Graaf <15962793+v4@users.noreply.github.com>
2021-02-03 00:50:27 +0100

    Detect CI tag for Bitbucket pipelines

--- packages/electron-publish/src/publisher.ts
+++ packages/electron-publish/src/publisher.ts
@@ -96,6 +96,6 @@ export abstract class HttpPublisher extends Publisher {
 }
 
 export function getCiTag() {
-  const tag = process.env.TRAVIS_TAG || process.env.APPVEYOR_REPO_TAG_NAME || process.env.CIRCLE_TAG || process.env.BITRISE_GIT_TAG || process.env.CI_BUILD_TAG
+  const tag = process.env.TRAVIS_TAG || process.env.APPVEYOR_REPO_TAG_NAME || process.env.CIRCLE_TAG || process.env.BITRISE_GIT_TAG || process.env.CI_BUILD_TAG || process.env.BITBUCKET_TAG
   return tag != null && tag.length > 0 ? tag : null
 }
