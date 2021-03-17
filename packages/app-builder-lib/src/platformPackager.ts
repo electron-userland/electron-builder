eb0d875dda99675c93c36456848b9b1c25cbd8e5
Mike Maietta <michaelmaietta@upwork.com>
2021-03-16 18:55:39 -0700

    feat: When archs are specified in the `win.targets` config AND `${arch}` template is present in artifact name, then also build individual installers in addition to the universal dist

--- packages/app-builder-lib/src/platformPackager.ts
+++ packages/app-builder-lib/src/platformPackager.ts
@@ -560,20 +560,18 @@ export abstract class PlatformPackager<DC extends PlatformSpecificBuildOptions> 
     skipDefaultArch = true,
     defaultArch?: string
   ): string {
-    let pattern = targetSpecificOptions == null ? null : targetSpecificOptions.artifactName
-    if (pattern == null) {
-      pattern = this.platformSpecificBuildOptions.artifactName || this.config.artifactName
-    }
-
-    if (pattern == null) {
-      // tslint:disable-next-line:no-invalid-template-strings
-      pattern = defaultPattern || "${productName}-${version}-${arch}.${ext}"
-    } else {
-      // https://github.com/electron-userland/electron-builder/issues/3510
-      // always respect arch in user custom artifact pattern
-      skipDefaultArch = this.platform === Platform.MAC
-    }
+    const pattern = this.artifactNamePattern(targetSpecificOptions, defaultPattern)
     return this.computeArtifactName(pattern, ext, skipDefaultArch && arch === defaultArchFromString(defaultArch) ? null : arch)
+  }
+
+  artifactNamePattern(targetSpecificOptions: TargetSpecificOptions | null | undefined, defaultPattern: string | undefined) {
+    return (
+      targetSpecificOptions?.artifactName ||
+      this.platformSpecificBuildOptions.artifactName ||
+      this.config.artifactName ||
+      defaultPattern ||
+      "${productName}-${version}-${arch}.${ext}"
+    )
   }
 
   expandArtifactBeautyNamePattern(targetSpecificOptions: TargetSpecificOptions | null | undefined, ext: string, arch?: Arch | null): string {
