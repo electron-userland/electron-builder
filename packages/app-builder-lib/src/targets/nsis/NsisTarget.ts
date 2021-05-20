014cbf5f98a25d6537f9f2b49e90cc401a2f3bce
Andy <56588504+vespasianvs@users.noreply.github.com>
2021-05-20 19:18:23 +0100

    fix: ${arch} causes a build fail (#5905)
    * Fix for Issue #5857

--- packages/app-builder-lib/src/targets/nsis/NsisTarget.ts
+++ packages/app-builder-lib/src/targets/nsis/NsisTarget.ts
@@ -119,12 +119,15 @@ export class NsisTarget extends Target {
 
   async finishBuild(): Promise<any> {
     try {
-      const { pattern } = this.packager.artifactPatternConfig(this.options, this.installerFilenamePattern)
-      const builds = new Set([this.archs])
-      if (pattern.includes("${arch}")) {
-        ;[...this.archs].forEach(([arch, appOutDir]) => builds.add(new Map<Arch, string>().set(arch, appOutDir)))
+      const { pattern } = this.packager.artifactPatternConfig(this.options, this.installerFilenamePattern);
+      const builds = new Set([this.archs]);
+      if (pattern.includes("${arch}") && this.archs.size > 1) {
+          ;[...this.archs].forEach(([arch, appOutDir]) => builds.add(new Map().set(arch, appOutDir)));
       }
-      await Promise.all([...builds].map(archs => this.buildInstaller(archs)))
+      const doBuildArchs = builds.values();
+      for (let archs of doBuildArchs) {
+          await this.buildInstaller(archs);
+      }
     } finally {
       await this.packageHelper.finishBuild()
     }
