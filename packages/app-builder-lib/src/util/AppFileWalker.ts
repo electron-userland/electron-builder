afcc5d5794f20c970fff1e5b728ebe377ee1d9ff
William J. Edney <bedney@technicalpursuit.com>
2021-05-21 10:41:08 -0500

    Fixes issue 3185
    This patch adds the ability to package either all or a select set of *submodules* node_modules directories in an Electron app.

--- packages/app-builder-lib/src/util/AppFileWalker.ts
+++ packages/app-builder-lib/src/util/AppFileWalker.ts
@@ -48,6 +48,20 @@ export abstract class FileCopyHelper {
 }
 
 function createAppFilter(matcher: FileMatcher, packager: Packager): Filter | null {
+    //  @ts-ignore
+    const includeSubNodeModules = (packager._configuration || {}).includeSubNodeModules;
+
+    //  for the places where we want to use the matcher to process node_modules
+    //  directories (i.e. when includeSubNodeModules is off), we have to
+    //  configure the matcher to act *exactly* like how it would according to
+    //  how the system currently works: filter out all node_modules directories
+    //  *except* the 'root' node_modules directory. We do this by finding the
+    //  universal '*/**' rule and splice in two rules that do that filtering.
+    //  then the user's rules that come after these rules can modify this
+    //  behavior to add specific node_modules directories in.
+    matcher.patterns.splice(
+      matcher.patterns.indexOf('*/**') + 1, 0, '!**/node_modules', '*/node_modules')
+
   if (packager.areNodeModulesHandledExternally) {
     return matcher.isEmpty() ? null : matcher.createFilter()
   }
@@ -62,17 +76,30 @@ function createAppFilter(matcher: FileMatcher, packager: Packager): Filter | nul
 
   const filter = matcher.createFilter()
   return (file, fileStat) => {
+    const matchesFilter = filter(file, fileStat)
     if (!nodeModulesFilter(file, fileStat)) {
-      return false
+      //  it's a node_modules directory
+
+      //  if includeSubNodeModules is true, then we just return true - we want
+      //  all of them
+      if (includeSubNodeModules) {
+        return true
+      }
     }
-    return filter(file, fileStat)
+    return matchesFilter
   }
 }
 
 /** @internal */
 export class AppFileWalker extends FileCopyHelper implements FileConsumer {
+  readonly includeSubNodeModules: boolean
+  readonly matcherFilter
+
   constructor(matcher: FileMatcher, packager: Packager) {
     super(addAllPatternIfNeed(matcher), createAppFilter(matcher, packager), packager)
+    //  @ts-ignore
+    this.includeSubNodeModules = (packager._configuration || {}).includeSubNodeModules;
+    this.matcherFilter = matcher.createFilter()
   }
 
   // noinspection JSUnusedGlobalSymbols
@@ -83,7 +110,21 @@ export class AppFileWalker extends FileCopyHelper implements FileConsumer {
       // but do not filter if we inside node_modules dir
       // update: solution disabled, node module resolver should support such setup
       if (file.endsWith(nodeModulesSystemDependentSuffix)) {
-        return false
+        //  it's a node_modules directory
+
+        //  if includeSubNodeModules is true, then we just do nothing - we
+        //  definitely want it. But if its false (the default), then we match
+        //  and if the match is false, we return false (but if the match is
+        //  true, we do nothing).
+        if (!this.includeSubNodeModules) {
+          const matchesFilter = this.matcherFilter(file, fileStat)
+          //  if it matched the patterns filter, then we just do nothing - we
+          //  want it. Otherwise, it didn't match the filter so we need to
+          //  return false here.
+          if (!matchesFilter) {
+            return false
+          }
+        }
       }
     } else {
       // save memory - no need to store stat for directory
