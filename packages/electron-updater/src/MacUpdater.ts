efd5a5564f05df8f1d6dbe25b3d740ff4c6d1dba
Guillermo Vaya <guivaya@gmail.com>
2021-09-17 22:23:58 +0200

    add authentication, prevent server from closing

--- packages/electron-updater/src/MacUpdater.ts
+++ packages/electron-updater/src/MacUpdater.ts
@@ -1,7 +1,7 @@
 import { AllPublishOptions, newError, safeStringifyJson } from "builder-util-runtime"
 import { stat } from "fs-extra"
 import { createReadStream } from "fs"
-import { createServer, IncomingMessage, ServerResponse } from "http"
+import { createServer, IncomingMessage, Server, ServerResponse } from "http"
 import { AddressInfo } from "net"
 import { AppAdapter } from "./AppAdapter"
 import { AppUpdater, DownloadUpdateOptions } from "./AppUpdater"
@@ -15,6 +15,8 @@ export class MacUpdater extends AppUpdater {
   private readonly nativeUpdater: AutoUpdater = require("electron").autoUpdater
 
   private squirrelDownloadedUpdate = false
+
+  private server?: Server
 
   constructor(options?: AllPublishOptions, app?: AppAdapter) {
     super(options, app)
@@ -96,19 +98,28 @@ export class MacUpdater extends AppUpdater {
     const log = this._logger
     const logContext = `fileToProxy=${zipFileInfo.url.href}`
     this.debug(`Creating proxy server for native Squirrel.Mac (${logContext})`)
-    const server = createServer()
+    if (typeof this.server !== 'undefined') {
+      this.server.close()
+    }
+    this.server = createServer()
     this.debug(`Proxy server for native Squirrel.Mac is created (${logContext})`)
-    server.on("close", () => {
+    this.server.on("close", () => {
       log.info(`Proxy server for native Squirrel.Mac is closed (${logContext})`)
     })
 
     // must be called after server is listening, otherwise address is null
+<<<<<<< HEAD
     function getServerUrl(): string {
       const address = server.address()
       if (typeof address === "string") {
         return address
       }
       return `${address?.address}:${address?.port}`
+=======
+    const getServerUrl = (): string => {
+      const address = this.server!.address() as AddressInfo
+      return `http://127.0.0.1:${address.port}`
+>>>>>>> 5120b9b0... add authentication, prevent server from closing
     }
 
     return await new Promise<Array<string>>((resolve, reject) => {
@@ -117,7 +128,7 @@ export class MacUpdater extends AppUpdater {
 
       // insecure random is ok
       const fileUrl = `/${Date.now().toString(16)}-${Math.floor(Math.random() * 9999).toString(16)}.zip`
-      server.on("request", (request: IncomingMessage, response: ServerResponse) => {
+      this.server!.on("request", (request: IncomingMessage, response: ServerResponse) => {
         // check for basic auth header
         if (!request.headers.authorization || request.headers.authorization.indexOf("Basic ") === -1) {
           response.statusCode = 401
@@ -158,13 +169,9 @@ export class MacUpdater extends AppUpdater {
 
         let errorOccurred = false
         response.on("finish", () => {
-          try {
-            setImmediate(() => server.close())
-          } finally {
-            if (!errorOccurred) {
-              this.nativeUpdater.removeListener("error", reject)
-              resolve([])
-            }
+          if (!errorOccurred) {
+            this.nativeUpdater.removeListener("error", reject)
+            resolve([])
           }
         })
 
@@ -189,7 +196,7 @@ export class MacUpdater extends AppUpdater {
 
       this.debug(`Proxy server for native Squirrel.Mac is starting to listen (${logContext})`)
 
-      server.listen(0, "127.0.0.1", () => {
+      this.server!.listen(0, "127.0.0.1", () => {
         this.debug(`Proxy server for native Squirrel.Mac is listening (address=${getServerUrl()}, ${logContext})`)
         this.nativeUpdater.setFeedURL({
           url: getServerUrl(),
@@ -214,6 +221,9 @@ export class MacUpdater extends AppUpdater {
   }
 
   quitAndInstall(): void {
+    if (typeof this.server !== "undefined") {
+      this.server.close()
+    }
     if (this.squirrelDownloadedUpdate) {
       // update already fetched by Squirrel, it's ready to install
       this.nativeUpdater.quitAndInstall()
