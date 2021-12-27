c6e282e60823b8685929267229c1a155668b067f
Robert Patrick <robert.patrick@oracle.com>
2021-12-27 12:14:10 -0600

    fix: specify protocol as https to complete proxy support fix
    Explicitly set the protocol to https on the request objects to allow publishing to work from behind a proxy server when the https_proxy environment variable is set.
    
    Closes #6286

--- packages/electron-publish/src/gitHubPublisher.ts
+++ packages/electron-publish/src/gitHubPublisher.ts
@@ -182,6 +182,7 @@ export class GitHubPublisher extends HttpPublisher {
       .doApiRequest(
         configureRequestOptions(
           {
+            protocol: "https:",
             hostname: parsedUrl.hostname,
             path: parsedUrl.path,
             method: "POST",
@@ -263,6 +264,7 @@ export class GitHubPublisher extends HttpPublisher {
       httpExecutor.request(
         configureRequestOptions(
           {
+            protocol: "https:",
             hostname: baseUrl.hostname,
             port: baseUrl.port as any,
             path: this.info.host != null && this.info.host !== "github.com" ? `/api/v3${path.startsWith("/") ? path : `/${path}`}` : path,
