93512a3991d48a3f02c72a1a68de51b72dcb1513
Mike Maietta <mmaietta@users.noreply.github.com>
2022-01-17 08:24:16 -0800

    Update push.sh script to push all related tags under the `builder` image

--- docker/push.sh
+++ docker/push.sh
@@ -2,29 +2,4 @@
 
 set -ex
 
-DATE=$(date +%m.%y)
-
-echo $DATE
-
-docker push \
-  electronuserland/builder:base \
-  "electronuserland/builder:base-$DATE"
-
-# Node 14
-docker push \
-  electronuserland/builder:14 \
-  "electronuserland/builder:14-$DATE" \
-  electronuserland/builder:latest
-
-docker push \
-  electronuserland/builder:14-wine \
-  "electronuserland/builder:14-wine-$DATE" \
-  electronuserland/builder:wine
-docker push \
-  electronuserland/builder:14-wine-mono \
-  "electronuserland/builder:14-wine-mono-$DATE" \
-  electronuserland/builder:wine-mono
-docker push \
-  electronuserland/builder:14-wine-chrome \
-  "electronuserland/builder:14-wine-chrome-$DATE" \
-  electronuserland/builder:wine-chrome
+docker image push --all-tags electronuserland/builder
