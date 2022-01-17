f95e66864124a224c29488bdbca1eedc2a180f3a
Mike Maietta <mmaietta@users.noreply.github.com>
2022-01-16 18:19:51 -0800

    chore(deploy): Enable docker deployments (#6560)

--- docker/push.sh
+++ docker/push.sh
@@ -6,25 +6,25 @@ DATE=$(date +%m.%y)
 
 echo $DATE
 
-# docker push \
-#   # electronuserland/builder:base \
-#   "electronuserland/builder:base-$DATE"
+docker push \
+  electronuserland/builder:base \
+  "electronuserland/builder:base-$DATE"
 
-# # Node 14
-# docker push \
-#   # electronuserland/builder:14 \
-#   "electronuserland/builder:14-$DATE" \
-#   # electronuserland/builder:latest
+# Node 14
+docker push \
+  electronuserland/builder:14 \
+  "electronuserland/builder:14-$DATE" \
+  electronuserland/builder:latest
 
-# docker push \
-#   # electronuserland/builder:14-wine \
-#   "electronuserland/builder:14-wine-$DATE" \
-#   # electronuserland/builder:wine
-# docker push \
-#   # electronuserland/builder:14-wine-mono \
-#   "electronuserland/builder:14-wine-mono-$DATE" \
-#   # electronuserland/builder:wine-mono
-# docker push \
-#   # electronuserland/builder:14-wine-chrome \
-#   "electronuserland/builder:14-wine-chrome-$DATE" \
-#   # electronuserland/builder:wine-chrome
+docker push \
+  electronuserland/builder:14-wine \
+  "electronuserland/builder:14-wine-$DATE" \
+  electronuserland/builder:wine
+docker push \
+  electronuserland/builder:14-wine-mono \
+  "electronuserland/builder:14-wine-mono-$DATE" \
+  electronuserland/builder:wine-mono
+docker push \
+  electronuserland/builder:14-wine-chrome \
+  "electronuserland/builder:14-wine-chrome-$DATE" \
+  electronuserland/builder:wine-chrome
