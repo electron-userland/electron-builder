21cbb572f311652ae44730eda294066157175ed2
Mike Maietta <mmaietta@outlook.com>
2023-03-23 22:09:38 -0700

    just use copySync

--- packages/app-builder-lib/src/electron/injectFFMPEG.ts
+++ packages/app-builder-lib/src/electron/injectFFMPEG.ts
@@ -11,11 +11,11 @@ const downloadFFMPEG = async (electronVersion: string, platform: ElectronPlatfor
   const ffmpegFileName = `ffmpeg-v${electronVersion}-${platform}-${arch}.zip`
   const url = `https://github.com/electron/electron/releases/download/v${electronVersion}/${ffmpegFileName}`
 
-  log.info({ url }, "downloading non-proprietary FFMPEG")
+  log.info({ file: ffmpegFileName }, "downloading non-proprietary FFMPEG")
   return getBin(ffmpegFileName, url)
 }
 
-const moveFFMPEG = (targetPath: string, platform: ElectronPlatformName) => (sourcePath: string) => {
+const copyFFMPEG = (targetPath: string, platform: ElectronPlatformName) => (sourcePath: string) => {
   let fileName = "libffmpeg.dll"
   if (["darwin", "mas"].includes(platform)) {
     fileName = "libffmpeg.dylib"
@@ -23,13 +23,13 @@ const moveFFMPEG = (targetPath: string, platform: ElectronPlatformName) => (sour
     fileName = "libffmpeg.so"
   }
 
-  const libTargetPath = path.resolve(targetPath, fileName)
   const libPath = path.resolve(sourcePath, fileName)
+  const libTargetPath = path.resolve(targetPath, fileName)
   log.info({ lib: libPath, target: libTargetPath }, "copying non-proprietary FFMPEG")
 
   // If we are copying to the source we can stop immediately
   if (libPath === libTargetPath) {
-    return
+    return libTargetPath
   }
 
   // If the source doesn't exist we have a problem
@@ -37,42 +37,8 @@ const moveFFMPEG = (targetPath: string, platform: ElectronPlatformName) => (sour
     throw new Error("Failed to find FFMPEG library file")
   }
 
-  return copyFile(libPath, libTargetPath)
-}
-
-const copyFile = (sourceFile: fs.PathLike, targetFile: fs.PathLike, deleteAfter = false) => {
-  return new Promise<Error | undefined>(resolve => {
-    let doneCalled = false
-
-    // If the target exists we should delete it
-    if (fs.existsSync(targetFile)) {
-      fs.unlinkSync(targetFile)
-    }
-
-    const done = (err: Error | undefined) => {
-      if (doneCalled) {
-        return
-      }
-      resolve(err)
-      doneCalled = true
-    }
-
-    const readStream = fs.createReadStream(sourceFile)
-    readStream.on("error", err => {
-      done(err)
-    })
-    const writeStream = fs.createWriteStream(targetFile)
-    writeStream.on("error", err => {
-      done(err)
-    })
-    writeStream.on("close", () => {
-      if (!doneCalled && deleteAfter) {
-        fs.unlinkSync(sourceFile)
-      }
-      done(undefined)
-    })
-    readStream.pipe(writeStream)
-  })
+  fs.copyFileSync(libPath, libTargetPath)
+  return libTargetPath
 }
 
 export default function injectFFMPEG(options: PrepareApplicationStageDirectoryOptions, electrionVersion: string) {
@@ -81,5 +47,5 @@ export default function injectFFMPEG(options: PrepareApplicationStageDirectoryOp
     libPath = path.resolve(options.appOutDir, "Electron.app/Contents/Frameworks/Electron Framework.framework/Versions/A/Libraries")
   }
 
-  return downloadFFMPEG(electrionVersion, options.platformName, options.arch).then(moveFFMPEG(libPath, options.platformName))
+  return downloadFFMPEG(electrionVersion, options.platformName, options.arch).then(copyFFMPEG(libPath, options.platformName))
 }
