1533501f999b364b656cdaa2048a1a7fd5e7c361
Mike Maietta <mmaietta@users.noreply.github.com>
2023-04-04 17:59:38 -0700

    feat: Moved `electronLanguages` to global config to support win/linux (#7516)
    * Removing `remoteBuild` functionality, it stopped working long ago when the service died.
    * Moved `electronLanguages` to global config to support win/linux

--- packages/app-builder-lib/src/linuxPackager.ts
+++ packages/app-builder-lib/src/linuxPackager.ts
@@ -1,9 +1,8 @@
-import { Arch, AsyncTaskManager, log } from "builder-util"
-import { DIR_TARGET, Platform, Target, TargetSpecificOptions } from "./core"
+import { Arch } from "builder-util"
+import { DIR_TARGET, Platform, Target } from "./core"
 import { LinuxConfiguration } from "./options/linuxOptions"
 import { Packager } from "./packager"
 import { PlatformPackager } from "./platformPackager"
-import { RemoteBuilder } from "./remoteBuilder/RemoteBuilder"
 import AppImageTarget from "./targets/AppImageTarget"
 import FlatpakTarget from "./targets/FlatpakTarget"
 import FpmTarget from "./targets/FpmTarget"
@@ -34,8 +33,6 @@ export class LinuxPackager extends PlatformPackager<LinuxConfiguration> {
       }
       return helper
     }
-
-    let remoteBuilder: RemoteBuilder | null = null
 
     for (const name of targets) {
       if (name === DIR_TARGET) {
@@ -68,53 +65,9 @@ export class LinuxPackager extends PlatformPackager<LinuxConfiguration> {
           return createCommonTarget(name, outDir, this)
         }
 
-        const target = new targetClass(name, this, getHelper(), outDir)
-        if (process.platform === "win32" || process.env._REMOTE_BUILD) {
-          if (remoteBuilder == null) {
-            remoteBuilder = new RemoteBuilder(this)
-          }
-          // return remoteBuilder.buildTarget(this, arch, appOutDir, this.packager)
-          return new RemoteTarget(target, remoteBuilder)
-        }
-        return target
+        return new targetClass(name, this, getHelper(), outDir)
       })
     }
-  }
-}
-
-class RemoteTarget extends Target {
-  private buildTaskManager = new AsyncTaskManager(this.remoteBuilder.packager.info.cancellationToken)
-
-  get options(): TargetSpecificOptions | null | undefined {
-    return this.target.options
-  }
-
-  get outDir(): string {
-    return this.target.outDir
-  }
-
-  constructor(private readonly target: Target, private readonly remoteBuilder: RemoteBuilder) {
-    super(
-      target.name,
-      true /* all must be scheduled in time (so, on finishBuild RemoteBuilder will have all targets added - so, we must set isAsyncSupported to true (resolved promise is returned)) */
-    )
-  }
-
-  async finishBuild() {
-    await this.buildTaskManager.awaitTasks()
-    await this.remoteBuilder.build()
-  }
-
-  build(appOutDir: string, arch: Arch) {
-    const promise = this.doBuild(appOutDir, arch)
-    this.buildTaskManager.addTask(promise)
-    return promise
-  }
-
-  private async doBuild(appOutDir: string, arch: Arch) {
-    log.info({ target: this.target.name, arch: Arch[arch] }, "scheduling remote build")
-    await this.target.checkOptions()
-    this.remoteBuilder.scheduleBuild(this.target, arch, appOutDir)
   }
 }
 
