05e36f2d148cee0900d5dda9880d455b22e467ab
alexey.barsov <alexey.barsov@jetbrains.com>
2023-10-23 17:46:20 +0200

    Fixing 3754 (a bit more info in log)

--- packages/electron-updater/src/MacUpdater.ts
+++ packages/electron-updater/src/MacUpdater.ts
@@ -99,7 +99,7 @@ export class MacUpdater extends AppUpdater {
     try {
       this.debug("Clearing quarantine bit")
       const result = execFileSync("xattr", ["-r", "-d", "com.apple.quarantine", downloadedFile])
-      log.info(`Quarantine bit is cleared: ${result}`)
+      log.info(`Quarantine bit is cleared from ${downloadedFile}: ${result?.trim()}`)
     } catch (e: any) {
       log.warn(`xattr shell command for clearing quarantine bit failed: ${e.message}`)
     }
