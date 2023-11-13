3f6757b41720c0740886325431c18be81a23ebbf
Mike Maietta <mmaietta@outlook.com>
2023-11-12 18:01:48 -0800

    fix: temporarily skip flaky test from transient dependency

--- test/src/mac/macPackagerTest.ts
+++ test/src/mac/macPackagerTest.ts
@@ -96,7 +96,7 @@ test.ifMac(
   )
 )
 
-test.ifMac("yarn two package.json w/ native module", () =>
+test.ifMac.skip("yarn two package.json w/ native module", () =>
   assertPack(
     "test-app-two-native-modules",
     {
