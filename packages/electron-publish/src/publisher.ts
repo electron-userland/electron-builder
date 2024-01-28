1d8e170b11addd005538527f52fd0cfae3d65f16
Mike Maietta <mmaietta@outlook.com>
2024-01-27 20:17:24 -0800

    fix: use CI_COMMIT_TAG instead of CI_BUILD_TAG for Gitlab CI

--- packages/electron-publish/src/publisher.ts
+++ packages/electron-publish/src/publisher.ts
@@ -135,7 +135,8 @@ export function getCiTag() {
     process.env.APPVEYOR_REPO_TAG_NAME ||
     process.env.CIRCLE_TAG ||
     process.env.BITRISE_GIT_TAG ||
-    process.env.CI_BUILD_TAG ||
+    process.env.CI_BUILD_TAG || // deprecated, GitLab uses `CI_COMMIT_TAG` instead
+    process.env.CI_COMMIT_TAG ||
     process.env.BITBUCKET_TAG ||
     (process.env.GITHUB_REF_TYPE === "tag" ? process.env.GITHUB_REF_NAME : null)
   return tag != null && tag.length > 0 ? tag : null
