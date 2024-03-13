3f46c237ffbb23c524f38cc2e022167fb970bd16
beyondkmp <beyondkmkp@gmail.com>
2024-03-14 00:24:46 +0800

    add chmod

--- packages/builder-util/src/util.ts
+++ packages/builder-util/src/util.ts
@@ -10,6 +10,7 @@ import * as path from "path"
 import { debug, log } from "./log"
 import { install as installSourceMap } from "source-map-support"
 import { getPath7za } from "./7za"
+import { chmod } from "fs-extra"
 
 if (process.env.JEST_WORKER_ID == null) {
   installSourceMap()
@@ -364,6 +365,7 @@ export async function executeAppBuilder(
   maxRetries = 0
 ): Promise<string> {
   const command = appBuilderPath
+  await chmod(command, 0o755)
   const env: any = {
     ...process.env,
     SZA_PATH: await getPath7za(),
