029d51e4fc63535334276d5ab04deb0de5a17d34
payne.fu <payne.fu@ringcentral.com>
2024-03-19 16:06:05 +0800

    add new dest

--- packages/app-builder-lib/src/asar/asarUtil.ts
+++ packages/app-builder-lib/src/asar/asarUtil.ts
@@ -6,13 +6,38 @@ import * as path from "path"
 import { AsarOptions } from "../options/PlatformSpecificBuildOptions"
 import { Packager } from "../packager"
 import { PlatformPackager } from "../platformPackager"
-import { getDestinationPath, ResolvedFileSet } from "../util/appFileCopier"
+import { ResolvedFileSet } from "../util/appFileCopier"
 import { AsarFilesystem, Node } from "./asar"
 import { hashFile, hashFileContents } from "./integrity"
 import { detectUnpackedDirs } from "./unpackDetector"
+import { NODE_MODULES_PATTERN } from "../fileTransformer"
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const pickle = require("chromium-pickle-js")
+
+function getDestinationPath(file: string, fileSet: ResolvedFileSet) {
+  if (file === fileSet.src) {
+    return fileSet.destination
+  } else {
+    const src = fileSet.src
+    const dest = fileSet.destination
+    if (file.length > src.length && file.startsWith(src) && file[src.length] === path.sep) {
+      return dest + file.substring(src.length)
+    } else {
+      // hoisted node_modules
+      // not lastIndexOf, to ensure that nested module (top-level module depends on) copied to parent node_modules, not to top-level directory
+      // project https://github.com/angexis/punchcontrol/commit/cf929aba55c40d0d8901c54df7945e1d001ce022
+      let index = file.indexOf(NODE_MODULES_PATTERN)
+      if (index < 0 && file.endsWith(`${path.sep}node_modules`)) {
+        index = file.length - 13
+      }
+      if (index < 0) {
+        throw new Error(`File "${file}" not under the source directory "${fileSet.src}"`)
+      }
+      return dest + file.substring(index + 13)
+    }
+  }
+}
 
 /** @internal */
 export class AsarPackager {
