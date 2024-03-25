d08baddd1acbf62ee663dc71a97eaa73640c75f9
payne.fu <payne.fu@ringcentral.com>
2024-03-26 00:16:15 +0800

    fix 7za with process.env.USE_SYSTEM_7ZA is true

--- packages/builder-util/src/7za.ts
+++ packages/builder-util/src/7za.ts
@@ -1,8 +1,11 @@
 import { path7x, path7za } from "7zip-bin"
 import { chmod } from "fs-extra"
+import * as fs from "fs"
 
 export async function getPath7za(): Promise<string> {
-  await chmod(path7za, 0o755)
+  if (fs.existsSync(path7za)) {
+    await chmod(path7za, 0o755)
+  }
   return path7za
 }
 
