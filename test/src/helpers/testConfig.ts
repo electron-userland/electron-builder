d9959c45dd2a6e7171994c7e95139de67a29491a
Mike Maietta <mmaietta@outlook.com>
2024-07-11 10:23:04 -0700

    downgrade electron to 23.3.10

--- test/src/helpers/testConfig.ts
+++ test/src/helpers/testConfig.ts
@@ -1,7 +1,7 @@
 import * as os from "os"
 import * as path from "path"
 
-export const ELECTRON_VERSION = "27.3.11"
+export const ELECTRON_VERSION = "23.3.10"
 
 export function getElectronCacheDir() {
   if (process.platform === "win32") {
