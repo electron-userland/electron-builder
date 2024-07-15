dfb757bce3c3f89ad6935a43770161a4a0ac4352
Mike Maietta <mmaietta@outlook.com>
2024-07-15 10:41:31 -0700

    add back semver check for linux ia32

--- test/src/helpers/downloadElectron.ts
+++ test/src/helpers/downloadElectron.ts
@@ -2,6 +2,7 @@ import { isCI as isCi } from "ci-info"
 import * as path from "path"
 import * as fs from "fs/promises"
 import { ELECTRON_VERSION, getElectronCacheDir } from "./testConfig"
+import { gte } from "semver"
 
 const executeAppBuilder: (options: any) => Promise<any> = require(path.join(__dirname, "../../..", "packages/builder-util")).executeAppBuilder
 
@@ -50,6 +51,11 @@ export function downloadAllRequiredElectronVersions(): Promise<any> {
     for (const arch of archs) {
       if (arch === "riscv64") {
         // No prebuilt electron for riscv64
+        continue
+      }
+      if (gte(ELECTRON_VERSION, "19.0.0") && platform === "linux" && arch === "ia32") {
+        // Chromium dropped support for ia32 linux binaries in 102.0.4999.0
+        // https://www.electronjs.org/docs/latest/breaking-changes#removed-ia32-linux-binaries
         continue
       }
       versions.push({
