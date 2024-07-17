d2092666c25e1736467b38c2a99beceee8cd1323
beyondkmp <beyondkmkp@gmail.com>
2024-07-17 18:29:55 +0800

    add nuspecTemplate

--- packages/electron-builder-squirrel-windows/src/SquirrelWindowsTarget.ts
+++ packages/electron-builder-squirrel-windows/src/SquirrelWindowsTarget.ts
@@ -1,6 +1,5 @@
 import { sanitizeFileName } from "app-builder-lib/out/util/filename"
 import { InvalidConfigurationError, log, isEmptyOrSpaces } from "builder-util"
-import { getBinFromUrl } from "app-builder-lib/out/binDownload"
 import { Arch, getArchSuffix, SquirrelWindowsOptions, Target } from "app-builder-lib"
 import { WinPackager } from "app-builder-lib/out/winPackager"
 import * as path from "path"
@@ -40,7 +39,7 @@ export default class SquirrelWindowsTarget extends Target {
     await createWindowsInstaller({
       ...distOptions,
       appDirectory: appOutDir,
-      outputDirectory: this.outDir,
+      outputDirectory: installerOutDir,
     })
 
     await packager.info.callArtifactBuildCompleted({
@@ -111,7 +110,7 @@ export default class SquirrelWindowsTarget extends Target {
       extraMetadataSpecs: projectUrl == null ? null : `\n    <projectUrl>${projectUrl}</projectUrl>`,
       copyright: appInfo.copyright,
       packageCompressionLevel: parseInt((process.env.ELECTRON_BUILDER_COMPRESSION_LEVEL || packager.compression === "store" ? 0 : 9) as any, 10),
-      vendorPath: await getBinFromUrl("Squirrel.Windows", "1.9.0", "zJHk4CMATM7jHJ2ojRH1n3LkOnaIezDk5FAzJmlSEQSiEdRuB4GGLCegLDtsRCakfHIVfKh3ysJHLjynPkXwhQ=="),
+      nuspecTemplate: path.join(__dirname, "template.nuspectemplate"),
       ...(this.options as any),
     }
 
