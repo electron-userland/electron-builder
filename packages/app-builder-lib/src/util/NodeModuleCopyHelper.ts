82460de4db5cceb2ba58f939715a83bde8864191
beyondkmp <beyondkmp@gmail.com>
2024-07-24 10:51:09 +0800

    use null instead of ""

--- packages/app-builder-lib/src/util/NodeModuleCopyHelper.ts
+++ packages/app-builder-lib/src/util/NodeModuleCopyHelper.ts
@@ -45,7 +45,7 @@ export class NodeModuleCopyHelper extends FileCopyHelper {
 
     const onNodeModuleFile = await resolveFunction(this.packager.appInfo.type, this.packager.config.onNodeModuleFile, "onNodeModuleFile")
 
-    const result: Array<string> = []
+    const result: Array<string | null> = []
     const queue: Array<string> = []
     const emptyDirs: Set<string> = new Set()
     const symlinkFiles: Map<string, number> = new Map()
@@ -158,9 +158,9 @@ export class NodeModuleCopyHelper extends FileCopyHelper {
       const resolvedPath = realpathSync(file)
       if (emptyDirs.has(resolvedPath)) {
         // delete symlink file if target is a empty dir
-        result[index] = ""
+        result[index] = null
       }
     }
-    return result.filter(it => it !== "")
+    return result.filter(it => it !== null) as Array<string>
   }
 }
