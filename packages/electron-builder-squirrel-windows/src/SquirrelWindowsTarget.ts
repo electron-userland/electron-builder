d36fefdfacca3aeb702c2aa8d5704c419f296d3c
beyondkmp <beyondkmkp@gmail.com>
2024-07-26 00:57:27 +0800

    update 7zip arch

--- packages/electron-builder-squirrel-windows/src/SquirrelWindowsTarget.ts
+++ packages/electron-builder-squirrel-windows/src/SquirrelWindowsTarget.ts
@@ -86,8 +86,8 @@ export default class SquirrelWindowsTarget extends Target {
 
   private select7zipArch(vendorDirectory: string, arch: Arch) {
     // Copy the 7-Zip executable for the configured architecture.
-    fs.copyFileSync(path.join(vendorDirectory, "7z-" + getArchSuffix(arch) + ".exe"), path.join(vendorDirectory, "7z.exe"))
-    fs.copyFileSync(path.join(vendorDirectory, "7z-" + getArchSuffix(arch) + ".dll"), path.join(vendorDirectory, "7z.dll"))
+    fs.copyFileSync(path.join(vendorDirectory, "7z-" + getArchSuffix(arch) || process.arch + ".exe"), path.join(vendorDirectory, "7z.exe"))
+    fs.copyFileSync(path.join(vendorDirectory, "7z-" + getArchSuffix(arch) || process.arch + ".dll"), path.join(vendorDirectory, "7z.dll"))
   }
 
   async computeEffectiveDistOptions(): Promise<SquirrelOptions> {
