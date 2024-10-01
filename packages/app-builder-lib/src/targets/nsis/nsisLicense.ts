7394f4b71c23f0eeeae862c0229fdb9e47daded3
beyondkmp <beyondkmkp@gmail.com>
2024-10-01 21:20:06 +0800

    Check if the file already starts with a UTF-8 BOM

--- packages/app-builder-lib/src/targets/nsis/nsisLicense.ts
+++ packages/app-builder-lib/src/targets/nsis/nsisLicense.ts
@@ -1,3 +1,4 @@
+import { log } from "builder-util"
 import { lcid } from "../../util/langs"
 import { getLicenseFiles, getNotLocalizedLicenseFile } from "../../util/license"
 import * as path from "path"
@@ -9,17 +10,26 @@ import * as fs from "fs"
 
 function convertFileToUtf8WithBOMSync(filePath: string): boolean {
   try {
-    const data = fs.readFileSync(filePath)
-    // UTF-8 BOM is EF BB BF
-    const BOM = Buffer.from([0xef, 0xbb, 0xbf])
-    const dataWithBOM = Buffer.concat([BOM, data])
-    fs.writeFileSync(filePath, dataWithBOM)
-    return true
-  } catch (err) {
-    console.error("Failed to convert file to UTF-8 with BOM: ", err)
-    return false
+    const data = fs.readFileSync(filePath);
+    const BOM = Buffer.from([0xef, 0xbb, 0xbf]);
+
+    // Check if the file already starts with a UTF-8 BOM
+    if (data.length >= 3 && data[0] === 0xef && data[1] === 0xbb && data[2] === 0xbf) {
+      log.info("File is already in UTF-8 with BOM format");
+      return true;
+    }
+
+    // If not, add the BOM
+    const dataWithBOM = Buffer.concat([BOM, data]);
+    fs.writeFileSync(filePath, dataWithBOM);
+    log.info("File successfully converted to UTF-8 with BOM");
+    return true;
+  } catch (err: any) {
+    log.error("Failed to convert file to UTF-8 with BOM: ", err.toString());
+    return false;
   }
 }
+
 export async function computeLicensePage(packager: WinPackager, options: NsisOptions, scriptGenerator: NsisScriptGenerator, languages: Array<string>): Promise<void> {
   const license = await getNotLocalizedLicenseFile(options.license, packager)
   if (license != null) {
