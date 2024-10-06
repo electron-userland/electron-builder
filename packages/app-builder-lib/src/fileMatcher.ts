8bf110d945f9a99daec47e999326e1102dc2facd
beyondkmp <beyondkmkp@gmail.com>
2024-10-06 16:06:24 +0800

    fix ut

--- packages/app-builder-lib/src/fileMatcher.ts
+++ packages/app-builder-lib/src/fileMatcher.ts
@@ -163,8 +163,6 @@ export function getMainFileMatchers(
     patterns.push("package.json")
   }
 
-  customFirstPatterns.push("!**/node_modules")
-
   // https://github.com/electron-userland/electron-builder/issues/1482
   const relativeBuildResourceDir = path.relative(matcher.from, buildResourceDir)
   if (relativeBuildResourceDir.length !== 0 && !relativeBuildResourceDir.startsWith(".")) {
