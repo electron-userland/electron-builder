b9c85e0ede8e8799014c459e79832a7e89285d7b
beyondkmp <beyondkmp@gmail.com>
2024-10-10 09:39:18 +0800

    add ut

--- test/src/HoistedNodeModuleTest.ts
+++ test/src/HoistedNodeModuleTest.ts
@@ -1,5 +1,7 @@
-import { assertPack, linuxDirTarget, verifyAsarFileTree } from "./helpers/packTester"
+import { assertPack, linuxDirTarget, verifyAsarFileTree,modifyPackageJson } from "./helpers/packTester"
 import { Platform } from "electron-builder"
+import { outputFile } from "fs-extra"
+import * as path from "path"
 
 test.ifAll("yarn workspace", () =>
   assertPack(
@@ -51,3 +53,62 @@ test.ifAll("yarn two package.json w/ native module", () =>
     }
   )
 )
+
+// test.ifAll("yarn pnp with hoisted config", () =>
+//   assertPack(
+//     "test-app-two-native-modules",
+//     {
+//       targets: linuxDirTarget,
+//     },
+//     {
+//       packed: context => verifyAsarFileTree(context.getResources(Platform.LINUX)),
+//     }
+//   )
+// )
+
+// test.ifAll("yarn pnp with hoisted config", () =>
+//   assertPack(
+//     "test-app-two-native-modules",
+//     {
+//       targets: linuxDirTarget,
+//     },
+//     {
+//       packed: context => verifyAsarFileTree(context.getResources(Platform.LINUX)),
+//     }
+//   )
+// )
+
+test.ifAll("pnpm without hoisted config", () =>
+  assertPack(
+    "test-app-pnpm-hoisted",
+    {
+      targets: linuxDirTarget,
+    },
+    {
+      isInstallDepsBefore: true,
+      projectDirCreated: projectDir => {
+        return Promise.all([
+          modifyPackageJson(projectDir, data => {
+            data.dependencies = {
+               "es5-ext": "0.10.53"
+            }
+          }),
+          outputFile(path.join(projectDir, "pnpm-lock.yaml"), ""),
+        ])
+      },
+      packed: context => verifyAsarFileTree(context.getResources(Platform.LINUX)),
+    }
+  )
+)
+
+// test.ifAll("pnpm with hoisted config", () =>
+//   assertPack(
+//     "test-app-pnpm-hoisted",
+//     {
+//       targets: linuxDirTarget,
+//     },
+//     {
+//       packed: context => verifyAsarFileTree(context.getResources(Platform.LINUX)),
+//     }
+//   )
+// )
