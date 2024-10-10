43e0c960bd883ff25e645cb0fe2ed3db65b5c82b
beyondkmp <beyondkmp@gmail.com>
2024-10-10 12:29:01 +0800

    add ut

--- test/src/HoistedNodeModuleTest.ts
+++ test/src/HoistedNodeModuleTest.ts
@@ -55,9 +55,10 @@ test.ifAll("yarn two package.json w/ native module", () =>
   )
 )
 
-test.ifAll("pnpm without hoisted config", () =>
+// https://github.com/electron-userland/electron-builder/issues/8493
+test.ifAll("pnpm es5-ext without hoisted config", () =>
   assertPack(
-    "test-app-pnmp-hoisted",
+    "test-app-hoisted",
     {
       targets: linuxDirTarget,
     },
@@ -75,6 +76,32 @@ test.ifAll("pnpm without hoisted config", () =>
       },
       packed: async context =>{
           expect(await readAsarJson(path.join(context.getResources(Platform.LINUX), "app.asar"), "node_modules/d/package.json")).toMatchSnapshot()
+      } 
+    }
+  )
+)
+
+// https://github.com/electron-userland/electron-builder/issues/8493
+test.ifAll("yarn parse-asn1", () =>
+  assertPack(
+    "test-app-hoisted",
+    {
+      targets: linuxDirTarget,
+    },
+    {
+      isInstallDepsBefore: true,
+      projectDirCreated: projectDir => {
+        return Promise.all([
+          modifyPackageJson(projectDir, data => {
+            data.dependencies = {
+               "parse-asn1":"5.1.7"
+            }
+          }),
+          outputFile(path.join(projectDir, "yarn.lock"), ""),
+        ])
+      },
+      packed: async context =>{
+          expect(await readAsarJson(path.join(context.getResources(Platform.LINUX), "app.asar"), "node_modules/asn1.js/package.json")).toMatchSnapshot()
       } 
     }
   )
