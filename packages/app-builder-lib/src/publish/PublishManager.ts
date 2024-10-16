327ac67c7dfd3baa34522618c681e123e1717490
Mike Maietta <mmaietta@outlook.com>
2024-10-15 21:34:22 -0700

    fix import

--- packages/app-builder-lib/src/publish/PublishManager.ts
+++ packages/app-builder-lib/src/publish/PublishManager.ts
@@ -37,7 +37,7 @@ import { PlatformPackager } from "../platformPackager"
 import { expandMacro } from "../util/macroExpander"
 import { WinPackager } from "../winPackager"
 import { createUpdateInfoTasks, UpdateInfoFileTask, writeUpdateInfoFiles } from "./updateInfoBuilder"
-import { MultiProgress } from "electron-publish/src/multiProgress"
+import { MultiProgress } from "electron-publish/out/multiProgress"
 
 const publishForPrWarning =
   "There are serious security concerns with PUBLISH_FOR_PULL_REQUEST=true (see the  CircleCI documentation (https://circleci.com/docs/1.0/fork-pr-builds/) for details)" +
