ec34ea283e8c500be796bcf737586af106679f59
Mike Maietta <mmaietta@outlook.com>
2024-10-27 08:36:21 -0700

    temporarily remove retry to identify unit test hanging in CI

--- packages/app-builder-lib/src/codeSign/windowsCodeSign.ts
+++ packages/app-builder-lib/src/codeSign/windowsCodeSign.ts
@@ -1,4 +1,4 @@
-import { log, retry } from "builder-util"
+import { log } from "builder-util"
 import { WindowsConfiguration } from "../options/winOptions"
 import { VmManager } from "../vm/vm"
 import { WinPackager } from "../winPackager"
@@ -11,8 +11,7 @@ export interface WindowsSignOptions {
 export async function signWindows(options: WindowsSignOptions, packager: WinPackager): Promise<boolean> {
   if (options.options.azureSignOptions) {
     log.info({ path: log.filePath(options.path) }, "signing with Azure Trusted Signing (beta)")
-    const packageManager = await packager.azureSignManager.value
-    return signWithRetry(async () => packageManager.signUsingAzureTrustedSigning(options))
+    return (await packager.azureSignManager.value).signUsingAzureTrustedSigning(options)
   }
 
   log.info({ path: log.filePath(options.path) }, "signing with signtool.exe")
@@ -35,24 +34,7 @@ export async function signWindows(options: WindowsSignOptions, packager: WinPack
   if (fields.length) {
     log.warn({ fields, reason: "please move to win.signtoolOptions.<field_name>" }, `deprecated field`)
   }
-  const packageManager = await packager.signtoolManager.value
-  return signWithRetry(async () => packageManager.signUsingSigntool(options))
-}
-
-function signWithRetry(signer: () => Promise<boolean>): Promise<boolean> {
-  return retry(signer, 3, 1000, 1000, 0, (e: any) => {
-    const message = e.message
-    if (
-      // https://github.com/electron-userland/electron-builder/issues/1414
-      message?.includes("Couldn't resolve host name") ||
-      // https://github.com/electron-userland/electron-builder/issues/8615
-      message?.includes("being used by another process.")
-    ) {
-      log.warn({ error: message }, "attempt to sign failed, another attempt will be made")
-      return true
-    }
-    return false
-  })
+  return (await packager.signtoolManager.value).signUsingSigntool(options)
 }
 
 export async function getPSCmd(vm: VmManager): Promise<string> {
