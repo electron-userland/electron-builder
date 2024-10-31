f84a0831d1d02b782ad07d4f7feff79d96dd45ec
Mike Maietta <mmaietta@users.noreply.github.com>
2024-10-31 09:48:30 -0700

    fix(win): add required `publisherName` field to Azure Trusted Signing options (#8650)

--- packages/app-builder-lib/src/codeSign/windowsCodeSign.ts
+++ packages/app-builder-lib/src/codeSign/windowsCodeSign.ts
@@ -9,34 +9,36 @@ export interface WindowsSignOptions {
 }
 
 export async function signWindows(options: WindowsSignOptions, packager: WinPackager): Promise<boolean> {
+  const packageManager = await packager.signingManager.value
   if (options.options.azureSignOptions) {
+    if (options.options.signtoolOptions) {
+      log.warn(null, "ignoring signtool options, using Azure Trusted Signing; please only configure one")
+    }
     log.info({ path: log.filePath(options.path) }, "signing with Azure Trusted Signing (beta)")
-    const packageManager = await packager.azureSignManager.value
-    return signWithRetry(async () => packageManager.signUsingAzureTrustedSigning(options))
+  } else {
+    log.info({ path: log.filePath(options.path) }, "signing with signtool.exe")
+    const deprecatedFields = {
+      sign: options.options.sign,
+      signDlls: options.options.signDlls,
+      signingHashAlgorithms: options.options.signingHashAlgorithms,
+      certificateFile: options.options.certificateFile,
+      certificatePassword: options.options.certificatePassword,
+      certificateSha1: options.options.certificateSha1,
+      certificateSubjectName: options.options.certificateSubjectName,
+      additionalCertificateFile: options.options.additionalCertificateFile,
+      rfc3161TimeStampServer: options.options.rfc3161TimeStampServer,
+      timeStampServer: options.options.timeStampServer,
+      publisherName: options.options.publisherName,
+    }
+    const fields = Object.entries(deprecatedFields)
+      .filter(([, value]) => !!value)
+      .map(([field]) => field)
+    if (fields.length) {
+      log.warn({ fields, reason: "please move to win.signtoolOptions.<field_name>" }, `deprecated field`)
+    }
   }
 
-  log.info({ path: log.filePath(options.path) }, "signing with signtool.exe")
-  const deprecatedFields = {
-    sign: options.options.sign,
-    signDlls: options.options.signDlls,
-    signingHashAlgorithms: options.options.signingHashAlgorithms,
-    certificateFile: options.options.certificateFile,
-    certificatePassword: options.options.certificatePassword,
-    certificateSha1: options.options.certificateSha1,
-    certificateSubjectName: options.options.certificateSubjectName,
-    additionalCertificateFile: options.options.additionalCertificateFile,
-    rfc3161TimeStampServer: options.options.rfc3161TimeStampServer,
-    timeStampServer: options.options.timeStampServer,
-    publisherName: options.options.publisherName,
-  }
-  const fields = Object.entries(deprecatedFields)
-    .filter(([, value]) => !!value)
-    .map(([field]) => field)
-  if (fields.length) {
-    log.warn({ fields, reason: "please move to win.signtoolOptions.<field_name>" }, `deprecated field`)
-  }
-  const packageManager = await packager.signtoolManager.value
-  return signWithRetry(async () => packageManager.signUsingSigntool(options))
+  return signWithRetry(async () => packageManager.signFile(options))
 }
 
 function signWithRetry(signer: () => Promise<boolean>): Promise<boolean> {
