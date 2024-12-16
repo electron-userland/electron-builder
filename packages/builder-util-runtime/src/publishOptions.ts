eacbbf593f6ea01a92ffb41d8d28ee5e4e480ea1
Timothy Lau <timothy-lau@outlook.com>
2024-12-17 07:28:02 +0800

    feat(publish): add `forcePathStyle` option to `S3Options` (#8741)

--- packages/builder-util-runtime/src/publishOptions.ts
+++ packages/builder-util-runtime/src/publishOptions.ts
@@ -362,6 +362,12 @@ export interface S3Options extends BaseS3Options {
    *  ${bucketname}.s3-accelerate.amazonaws.com
    */
   readonly accelerate?: boolean
+
+  /**
+   * When true, force a path-style endpoint to be used where the bucket name is part of the path.
+   * [Path-style Access](https://docs.aws.amazon.com/AmazonS3/latest/userguide/VirtualHosting.html#path-style-access)
+   */
+  readonly forcePathStyle?: boolean
 }
 
 /**
