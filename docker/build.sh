e48e727728086a744bb6ab5fe735d1762024667e
Mike Maietta <mmaietta@outlook.com>
2025-01-16 19:55:24 -0800

    add node 22 image

--- docker/build.sh
+++ docker/build.sh
@@ -9,11 +9,11 @@ docker build -t electronuserland/builder:base -t "electronuserland/builder:base-
 ## NOTE: Order the latest to oldest versions. The most recent node LTS should be tagged as the latest image
 
 # Node 22
-# docker build --build-arg NODE_VERSION=22.13.0 --build-arg IMAGE_VERSION=base-$DATE -t electronuserland/builder:22 -t "electronuserland/builder:22-$DATE" -t electronuserland/builder:latest docker/node
+docker build --build-arg NODE_VERSION=22.13.0 --build-arg IMAGE_VERSION=base-$DATE -t electronuserland/builder:22 -t "electronuserland/builder:22-$DATE" -t electronuserland/builder:latest docker/node
 
-# docker build --build-arg IMAGE_VERSION=20-$DATE -t electronuserland/builder:22-wine -t "electronuserland/builder:22-wine-$DATE" -t electronuserland/builder:wine docker/wine
-# docker build --build-arg IMAGE_VERSION=20-wine-$DATE -t electronuserland/builder:22-wine-mono -t "electronuserland/builder:22-wine-mono-$DATE" -t electronuserland/builder:wine-mono docker/wine-mono
-# docker build --build-arg IMAGE_VERSION=20-wine-$DATE -t electronuserland/builder:22-wine-chrome -t "electronuserland/builder:22-wine-chrome-$DATE" -t electronuserland/builder:wine-chrome docker/wine-chrome
+docker build --build-arg IMAGE_VERSION=20-$DATE -t electronuserland/builder:22-wine -t "electronuserland/builder:22-wine-$DATE" -t electronuserland/builder:wine docker/wine
+docker build --build-arg IMAGE_VERSION=20-wine-$DATE -t electronuserland/builder:22-wine-mono -t "electronuserland/builder:22-wine-mono-$DATE" -t electronuserland/builder:wine-mono docker/wine-mono
+docker build --build-arg IMAGE_VERSION=20-wine-$DATE -t electronuserland/builder:22-wine-chrome -t "electronuserland/builder:22-wine-chrome-$DATE" -t electronuserland/builder:wine-chrome docker/wine-chrome
 
 # Node 20
 docker build --build-arg NODE_VERSION=20.18.1 --build-arg IMAGE_VERSION=base-$DATE -t electronuserland/builder:20 -t "electronuserland/builder:20-$DATE" docker/node
