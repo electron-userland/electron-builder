93d38d799614a5b76e5acfb41fd30ed585f891f8
Mike Maietta <mmaietta@outlook.com>
2025-01-18 10:16:44 -0800

    add additional docker tagging to deploy script

--- docker/build.sh
+++ docker/build.sh
@@ -8,15 +8,6 @@ DATE=$(date +%m.%y)
 
 docker build -t electronuserland/builder:base -t "electronuserland/builder:base-$DATE" docker/base
 
-## NOTE: Order the latest to oldest versions. The most recent node LTS should be tagged as the latest image
-
-# # Node 22
-# docker build --build-arg NODE_VERSION=22.13.0 --build-arg IMAGE_VERSION=base-$DATE -t electronuserland/builder:22 -t "electronuserland/builder:22-$DATE" -t electronuserland/builder:latest docker/node
-
-# docker build --build-arg IMAGE_VERSION=22-$DATE -t electronuserland/builder:22-wine -t "electronuserland/builder:22-wine-$DATE" -t electronuserland/builder:wine docker/wine
-# docker build --build-arg IMAGE_VERSION=22-wine-$DATE -t electronuserland/builder:22-wine-mono -t "electronuserland/builder:22-wine-mono-$DATE" -t electronuserland/builder:wine-mono docker/wine-mono
-# docker build --build-arg IMAGE_VERSION=22-wine-$DATE -t electronuserland/builder:22-wine-chrome -t "electronuserland/builder:22-wine-chrome-$DATE" -t electronuserland/builder:wine-chrome docker/wine-chrome
-
 docker build \
   --build-arg NODE_VERSION="$NODE_VERSION" \
   --build-arg IMAGE_VERSION="base-$DATE" \
