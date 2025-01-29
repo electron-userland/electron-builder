c128e0438d1ccb6911418d26b307a94838eddd94
Mike Maietta <mmaietta@outlook.com>
2025-01-29 09:00:28 -0800

    hurray! we have a script to autogenerate rollup configs

--- packages/builder-util-runtime/rollup.config.ts
+++ packages/builder-util-runtime/rollup.config.ts
@@ -1,9 +1,9 @@
-import typescript from "@rollup/plugin-typescript"
 import commonjs from "@rollup/plugin-commonjs"
+import typescript from "@rollup/plugin-typescript"
 import { defineConfig } from "rollup"
 
 export default defineConfig({
-  input: "./src/index.ts",
+  input: "src/index.ts",
   output: {
     dir: "./out",
     format: "cjs",
--- packages/dmg-builder/rollup.config.ts
+++ packages/dmg-builder/rollup.config.ts
@@ -1,11 +1,12 @@
 import commonjs from "@rollup/plugin-commonjs"
 import typescript from "@rollup/plugin-typescript"
+import { defineConfig } from "rollup"
 
-export default {
+export default defineConfig({
   input: "src/dmgUtil.ts",
   output: {
     dir: "./out",
     format: "cjs",
   },
   plugins: [commonjs({ extensions: [".js", ".ts"] }), typescript({ tsconfig: "./tsconfig.json" })],
-}
+})
