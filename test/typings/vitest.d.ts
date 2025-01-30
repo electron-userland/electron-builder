eee9d9cdf0a6ea6aaba30054d2f9f9e9249cd626
Mike Maietta <mmaietta@outlook.com>
2025-01-29 18:38:02 -0800

    add typings

---
+++ test/typings/vitest.d.ts
@@ -0,0 +1,46 @@
+import * as vitest from 'vitest'
+
+type Test = typeof import('vitest')['test']
+
+interface CustomMatchers<R = unknown> {
+  toMatchObject: (object: any) => R
+}
+interface CustomTestMatcher extends Test {
+  ifNotWindows: CustomTestMatcher
+  ifMac: CustomTestMatcher
+  ifNotMac: CustomTestMatcher
+  ifWindows: CustomTestMatcher
+  ifNotCi: CustomTestMatcher
+  ifCi: CustomTestMatcher
+  ifNotCiMac: CustomTestMatcher
+  ifNotCiWin: CustomTestMatcher
+  ifDevOrWinCi: CustomTestMatcher
+  ifWinCi: CustomTestMatcher
+  ifDevOrLinuxCi: CustomTestMatcher
+  ifLinux: CustomTestMatcher
+  ifLinuxOrDevMac: CustomTestMatcher
+  ifAll: CustomTestMatcher
+  ifEnv: (envVar: any) => CustomTestMatcher
+}
+
+declare module 'vitest' {
+  interface Assertion<T = any> extends CustomMatchers<T> {}
+  interface AsymmetricMatchersContaining extends CustomMatchers {}
+
+  interface TestAPI extends CustomTestMatcher {}
+  type TestAPI = CustomTestMatcher
+
+  interface Describe {
+    ifAll: vitest.Describe
+  }
+
+  interface Matchers {
+    toMatchObject(object: any)
+  }
+}
+
+declare global {
+  const it: CustomTestMatcher
+  const test: CustomTestMatcher
+  const describe: typeof import('vitest')['describe']
+}
