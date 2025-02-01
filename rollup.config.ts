3eade5d34397e3310789ebd86594bf05c4a84b85
Mike Maietta <mmaietta@outlook.com>
2025-02-01 05:40:27 -0800

    tmp save

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,6 +1,9 @@
 import typescript from "@rollup/plugin-typescript"
 import { defineConfig } from "rollup"
 import * as glob from "glob"
+import { createRequire } from "node:module"
+import path from "node:path"
+import * as fs from "fs"
 
 const packageMap = [
   {
@@ -53,29 +56,59 @@ const packageMap = [
   },
 ]
 
+const outputOptions = {
+  exports: "named",
+  preserveModules: true,
+  // Ensures that CJS default exports are imported properly (based on __esModule)
+  // If needed, can switch to 'compat' which checks for .default prop on the default export instead
+  // see https://rollupjs.org/configuration-options/#output-interop
+  interop: "auto",
+}
+
 export default () => {
   const outDir = "out"
   return packageMap.map(pkg => {
-    const dir = `packages/${pkg.package}/${outDir}`
-    const input = glob.sync(`packages/${pkg.package}/${pkg.entry}`, { ignore: [dir, "**/*/*.d.ts"] })
+    const dir = p => `packages/${pkg.package}/${p}`
+    // const input = glob.sync(`packages/${pkg.package}/${pkg.entry}`, { ignore: [dir, "**/*/*.d.ts"] })
+
+    const packageJson = JSON.parse(fs.readFileSync(dir("package.json"), "utf-8"))
+    // const input = filesGlob.reduce((prev, curr) => {
+
+    // })
+    const input = packageJson.main.replace("out/", "src/").replace(".js", ".ts")
     return defineConfig({
       input,
       treeshake: false,
-      output: {
-        dir: dir,
-        format: "cjs",
-        sourcemap: true,
-        preserveModules: true, // Keep files separates instead of one bundled file
-      },
+      output: [
+        {
+          dir: path.join(dir(outDir), "esm"),
+          format: "esm",
+          exports: "named",
+          preserveModules: true,
+          // Ensures that CJS default exports are imported properly (based on __esModule)
+          // If needed, can switch to 'compat' which checks for .default prop on the default export instead
+          // see https://rollupjs.org/configuration-options/#output-interop
+          interop: "auto",
+        },
+        {
+          dir: path.join(dir(outDir), "cjs"),
+          format: "cjs",
+          exports: "named",
+          preserveModules: true,
+          interop: "auto",
+        },
+      ],
       plugins: [
         typescript({
           tsconfig: `packages/${pkg.package}/tsconfig.json`,
           checkJs: true,
-          declaration: true,
-          declarationDir: dir,
+          // declaration: true,
+          // declarationDir: path.join(dir, "types"),
+          // outDir: dir,
           sourceMap: true,
         }),
       ],
+      external: id => !/^[./]/.test(id),
     })
   })
 }
