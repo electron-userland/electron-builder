df8979aeefcb7767d2915a11510874765ba1f6d8
Mike Maietta <mmaietta@outlook.com>
2025-02-03 02:29:08 -0800

    schema works!

--- scripts/generate-schema.ts
+++ scripts/generate-schema.ts
@@ -1,19 +1,10 @@
-import * as path from "path"
 import * as fs from "fs"
-import * as TJS from "typescript-json-schema"
+import * as path from "path"
 import { TypeScript } from "typedoc"
-
-// optionally pass argument to schema generator
-const settings: TJS.PartialArgs = {
-  required: true,
-  noExtraProps: true,
-  typeOfKeyword: true,
-  strictNullChecks: true,
-  skipLibCheck: true,
-}
+import * as TJS from "typescript-json-schema"
 
 const rootDir = path.resolve(__dirname, "../packages")
-// optionally pass ts compiler options
+
 const compilerOptions: TJS.CompilerOptions = {
   // outDir: "out",
   baseUrl: rootDir,
@@ -38,22 +29,53 @@ const compilerOptions: TJS.CompilerOptions = {
   newLine: TypeScript.NewLineKind.LineFeed,
 
   noEmitOnError: true,
-  // typeRoots: ["./packages/app-builder-lib/typings", "./node_modules/@types/"],
-  types: [
-    "../../typings",
-    // "../typings",
-    // "./src/typings",
-    // "./app-builder-lib/src/typings",
-    // "./packages/typings",
-    // path.join(rootDir, "../typings"),
-    // "typings/flatpak-bundler.d.ts"
-    // "./packages/app-builder-lib/typings"
+  types: ["../../typings"],
+}
+
+// schema generator args
+const settings: TJS.PartialArgs = {
+  required: true,
+  noExtraProps: true,
+  typeOfKeyword: true,
+  strictNullChecks: true,
+  skipLibCheck: true,
+}
+
+const definitionFile = path.resolve(rootDir, "app-builder-lib/src/configuration.ts")
+const program = TJS.getProgramFromFiles([definitionFile], compilerOptions, rootDir)
+const generator = TJS.buildGenerator(program, settings)
+const schema = TJS.generateSchema(program, "Configuration", settings, [], generator!)
+
+const PlugDescriptor: any = schema!.definitions!.PlugDescriptor
+PlugDescriptor.additionalProperties!.anyOf![0] = {
+  type: "object",
+}
+
+const OutgoingHttpHeaders: any = schema!.definitions!.OutgoingHttpHeaders
+OutgoingHttpHeaders.additionalProperties = {
+  anyOf: [
+    {
+      items: {
+        type: "string",
+      },
+      type: "array",
+    },
+    {
+      type: ["string", "number"],
+    },
   ],
 }
 
-const program = TJS.getProgramFromFiles([path.resolve(rootDir, "app-builder-lib/src/configuration.ts")], compilerOptions, rootDir)
-const generator = TJS.buildGenerator(program, settings)
-const schema = TJS.generateSchema(program, "CommonConfiguration", settings, [], generator!)
+const SnapOptions: any = schema!.definitions!.SnapOptions
+SnapOptions.properties.environment!.anyOf![0] = {
+  additionalProperties: { type: "string" },
+  type: "object",
+}
+
+schema!.properties!["$schema"] = {
+  description: "JSON Schema for this document.",
+  type: ["null", "string"],
+}
 
 const schemaFile = path.join(__dirname, "../packages/app-builder-lib/scheme.json")
 fs.writeFileSync(schemaFile, JSON.stringify(schema, null, 2))
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,2 +1,4 @@
 import "./chromium-pickle-js"
 import "./flatpak-bundler"
+import "./minimatch"
+import "./path-sort"
