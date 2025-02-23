e301612e7f00d979769a0f529eb008fc483b6c5a
beyondkmp <beyondkmp@gmail.com>
2025-02-23 18:00:39 +0800

    add test info

--- packages/app-builder-lib/src/util/plist.ts
+++ packages/app-builder-lib/src/util/plist.ts
@@ -2,6 +2,8 @@ import { build } from "plist"
 import * as fs from "fs"
 
 export async function savePlistFile(path: string, data: any): Promise<void> {
+  console.log("before plist test is here", JSON.stringify(data))
   const plist = build(data)
+  console.log("plist test is here", plist)
   await fs.promises.writeFile(path, plist)
 }
