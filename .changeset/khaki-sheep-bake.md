8115e4aadecbdeea12336a575f5f2ae78ce8985b
Mike Maietta <mmaietta@outlook.com>
2025-02-26 18:27:23 -0800

    chore(refactor): refactoring code to reduce cyclical imports in order to prep for migration to rollup + vite (which have much more strict module resolutions)

---
+++ .changeset/khaki-sheep-bake.md
@@ -0,0 +1,5 @@
+---
+"electron-updater": patch
+---
+
+chore(refactor): refactoring code to reduce cyclical imports in order to migrate to rollup + vite (which have much more strict module resolutions)
--- packages/electron-updater/src/DownloadedUpdateHelper.ts
+++ packages/electron-updater/src/DownloadedUpdateHelper.ts
@@ -3,7 +3,8 @@ import { createHash } from "crypto"
 import { createReadStream } from "fs"
 // @ts-ignore
 import * as isEqual from "lodash.isequal"
-import { Logger, ResolvedUpdateFileInfo } from "./main"
+import { ResolvedUpdateFileInfo } from "./exports"
+import { Logger } from "./exports"
 import { pathExists, readJson, emptyDir, outputJson, unlink } from "fs-extra"
 import * as path from "path"
 
