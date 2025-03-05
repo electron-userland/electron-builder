419427b98c9058862cced5d00441b5d3334e067b
beyondkmp <beyondkmp@gmail.com>
2025-03-05 20:42:51 +0800

    support yarn cherry

---
+++ packages/app-builder-lib/src/node-module-collector/yarn2NodeModulesCollector.ts
@@ -0,0 +1,74 @@
+import { Lazy } from "lazy-val"
+import { NodeModulesCollector } from "./nodeModulesCollector"
+import { DependencyTree, NpmDependency, ParsedDependencyTree } from "./types"
+import { log } from "builder-util"
+
+export class Yarn2NodeModulesCollector extends NodeModulesCollector<NpmDependency, string> {
+  constructor(rootDir: string) {
+    super(rootDir)
+  }
+
+  public readonly pmCommand = new Lazy<string>(() => Promise.resolve(process.platform === "win32" ? "yarn.cmd" : "yarn"))
+  public readonly installOptions = Promise.resolve({
+    cmd: process.platform === "win32" ? "yarn.cmd" : "yarn",
+    args: ["install", "--frozen-lockfile"],
+    lockfile: "yarn.lock",
+  })
+
+  protected getArgs(): string[] {
+    return ["list", "-a", "--include", "prod", "--include", "optional", "--omit", "dev", "--json", "--long", "--silent"]
+  }
+
+  protected extractRelevantData(npmTree: NpmDependency): NpmDependency {
+    const tree = super.extractRelevantData(npmTree)
+    const { optionalDependencies, _dependencies } = npmTree
+    return { ...tree, optionalDependencies, _dependencies }
+  }
+
+  protected extractProductionDependencyTree(tree: NpmDependency): DependencyTree {
+    const _deps = tree._dependencies ?? {}
+
+    let deps = tree.dependencies ?? {}
+    let implicitDependenciesInjected = false
+
+    if (Object.keys(_deps).length > 0 && Object.keys(deps).length === 0) {
+      log.debug({ name: tree.name, version: tree.version }, "injecting implicit _dependencies")
+      deps = this.allDependencies.get(`${tree.name}@${tree.version}`)?.dependencies ?? {}
+      implicitDependenciesInjected = true
+    }
+
+    const dependencies = Object.entries(deps).reduce<DependencyTree["dependencies"]>((acc, curr) => {
+      const [packageName, dependency] = curr
+      if (!_deps[packageName] || Object.keys(dependency).length === 0) {
+        return acc
+      }
+      if (implicitDependenciesInjected) {
+        const { name, version, path, workspaces } = dependency
+        const simplifiedTree: ParsedDependencyTree = { name, version, path, workspaces }
+        return {
+          ...acc,
+          [packageName]: { ...simplifiedTree, implicitDependenciesInjected },
+        }
+      }
+      return {
+        ...acc,
+        [packageName]: this.extractProductionDependencyTree(dependency),
+      }
+    }, {})
+
+    const { name, version, path: packagePath, workspaces } = tree
+    const depTree: DependencyTree = {
+      name,
+      version,
+      path: packagePath,
+      workspaces,
+      dependencies,
+      implicitDependenciesInjected,
+    }
+    return depTree
+  }
+
+  protected parseDependenciesTree(jsonBlob: string): NpmDependency {
+    return JSON.parse(jsonBlob)
+  }
+}
