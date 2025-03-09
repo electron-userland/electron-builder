467e52294fff5d580a8c6a803a749eac987a6e10
Mike Maietta <mmaietta@outlook.com>
2025-03-09 14:55:40 -0700

    compiles and `only` tsdoc works again

--- test/src/vitest/vitest-setup.ts
+++ test/src/vitest/vitest-setup.ts
@@ -1,6 +1,6 @@
 import { isCI as isCi } from "ci-info"
-import { afterEach, test, vitest } from "vitest"
-import { CustomTestMatcher } from "./vitest"
+import { afterEach, vitest } from "vitest"
+import * as wrappedTest from "@test/vitest/vitest-test-wrapper"
 
 afterEach(() => {
   vitest.clearAllMocks()
@@ -9,6 +9,8 @@ afterEach(() => {
 const isWindows = process.platform === "win32"
 const isMac = process.platform === "darwin"
 const isLinux = process.platform === "linux"
+
+const test: any = wrappedTest.test
 
 // test.ifEnv = test.runIf
 // test.ifMac = test.runIf(isMac)
@@ -29,50 +31,72 @@ const isLinux = process.platform === "linux"
 // test.ifLinux = test.runIf(isLinux)
 // test.ifLinuxOrDevMac = test.runIf(isLinux || (!isCi && isMac))
 
-// const skip = test.skip
+// test = {
+//   ...test,
+//   ifEnv: test.runIf,
+//   ifMac: test.runIf(isMac),
+//   ifNotMac: test.runIf(!isMac),
 
-// test.ifEnv = test.runIf
+//   ifWindows: test.runIf(isWindows),
+//   ifNotWindows: test.runIf(!isWindows),
+//   ifWinCi: test.runIf(isCi && isWindows),
 
-// test.ifMac = isMac ? test : skip
+//   ifCi: test.runIf(isCi),
+//   ifNotCi: test.runIf(!isCi),
+//   ifNotCiMac: test.runIf(!isCi && !isMac),
+//   ifNotCiWin: test.runIf(!isCi && !isWindows),
 
-// test.ifNotWindows = isWindows ? skip : test
-// test.ifNotMac = isMac ? skip : test
-// test.ifNotWindows.ifNotCiMac = isCi && isMac ? skip : test
+//   ifDevOrWinCi: test.runIf(!isCi || isWindows),
+//   ifDevOrLinuxCi: test.runIf(!isCi || isLinux),
 
-// test.ifWindows = isWindows ? test : skip
-
-// skip.ifMac = skip
-// skip.ifLinux = skip
-// skip.ifWindows = skip
-
-// skip.ifNotWindows = skip
-// skip.ifNotMac = skip
-
-// skip.ifCi = skip
-// skip.ifNotCi = skip
-// skip.ifNotCiMac = skip
-// skip.ifNotCiWin = skip
-// skip.ifDevOrWinCi = skip
-// skip.ifDevOrLinuxCi = skip
-// skip.ifWinCi = skip
-// skip.ifLinuxOrDevMac = skip
-
-// if (isCi) {
-//   test.ifCi = test
-//   test.ifNotCi = skip
-// } else {
-//   test.ifCi = skip
-//   test.ifNotCi = test
+//   ifLinux: test.runIf(isLinux),
+//   ifLinuxOrDevMac: test.runIf(isLinux || (!isCi && isMac)),
 // }
 
-// test.ifNotCiMac = isCi && isMac ? skip : test
-// test.ifNotCiWin = isCi && isWindows ? skip : test
+const skip = test.skip
 
-// test.ifDevOrWinCi = !isCi || isWindows ? test : skip
-// test.ifDevOrLinuxCi = !isCi || isLinux ? test : skip
-// test.ifWinCi = isCi && isWindows ? test : skip
-// test.ifLinux = isLinux ? test : skip
-// test.ifLinuxOrDevMac = isLinux || (!isCi && isMac) ? test : skip
+test.ifEnv = test.runIf
+
+test.ifMac = isMac ? test : skip
+
+test.ifNotWindows = isWindows ? skip : test
+test.ifNotMac = isMac ? skip : test
+test.ifNotWindows.ifNotCiMac = isCi && isMac ? skip : test
+
+test.ifWindows = isWindows ? test : skip
+
+skip.ifMac = skip
+skip.ifLinux = skip
+skip.ifWindows = skip
+
+skip.ifNotWindows = skip
+skip.ifNotMac = skip
+
+skip.ifCi = skip
+skip.ifNotCi = skip
+skip.ifNotCiMac = skip
+skip.ifNotCiWin = skip
+skip.ifDevOrWinCi = skip
+skip.ifDevOrLinuxCi = skip
+skip.ifWinCi = skip
+skip.ifLinuxOrDevMac = skip
+
+if (isCi) {
+  test.ifCi = test
+  test.ifNotCi = skip
+} else {
+  test.ifCi = skip
+  test.ifNotCi = test
+}
+
+test.ifNotCiMac = isCi && isMac ? skip : test
+test.ifNotCiWin = isCi && isWindows ? skip : test
+
+test.ifDevOrWinCi = !isCi || isWindows ? test : skip
+test.ifDevOrLinuxCi = !isCi || isLinux ? test : skip
+test.ifWinCi = isCi && isWindows ? test : skip
+test.ifLinux = isLinux ? test : skip
+test.ifLinuxOrDevMac = isLinux || (!isCi && isMac) ? test : skip
 
 delete process.env.CSC_NAME
 if (process.env.TEST_APP_TMP_DIR == null) {
--- test/src/vitest/vitest-test-wrapper.ts
+++ test/src/vitest/vitest-test-wrapper.ts
@@ -1,14 +1,6 @@
-import { isCI as isCi } from "ci-info"
-import { createExpect, TestContext } from "vitest"
-import { createChainable, createTaskCollector, getCurrentSuite, setFn } from "vitest/suite"
+import { TaskCustomOptions, TestContext } from "vitest"
+import { createTaskCollector, getCurrentSuite } from "vitest/suite"
 import { CustomTestMatcher } from "./vitest"
-// import "./typings/vitest"
-
-// type Test = (...args) => void | Promise<void>
-
-const isWindows = process.platform === "win32"
-const isMac = process.platform === "darwin"
-const isLinux = process.platform === "linux"
 
 // Handle electron-builder flaky (due to parallel file operations such as hdiutil and EPERM file locks) tests by retrying
 // This is a workaround until we can find a better solution. For now, just slot in a 500ms delay and retry once.
@@ -27,162 +19,25 @@ const isSupposedToRetry = (errorMessage: string, alreadyRetried: boolean) => {
   return false
 }
 
-const customTestMatchers = [
-  "ifEnv",
-  "ifMac",
-  "ifNotWindows",
-  "ifNotMac",
-  "ifNotCiMac",
-  "ifWindows",
-  "ifLinux",
-  "ifCi",
-  "ifNotCi",
-  "ifNotCiWin",
-  "ifDevOrWinCi",
-  "ifDevOrLinuxCi",
-  "ifWinCi",
-  "ifLinuxOrDevMac",
-]
+export const test = createTaskCollector(function (this: TaskCustomOptions, name, runTest, timeout) {
+  const suite = getCurrentSuite()
 
-const testMatchers = ["concurrent", "sequential", "skip", "only", "todo", "fails", "runIf"]
-
-export const test = createChainable(
-  [
-    "concurrent",
-    "sequential",
-    "skip",
-    "only",
-    "todo",
-    "fails",
-    "runIF",
-    "ifEnv",
-    "ifMac",
-    "ifNotWindows",
-    "ifNotMac",
-    "ifNotCiMac",
-    "ifWindows",
-    "ifLinux",
-    "ifCi",
-    "ifNotCi",
-    "ifNotCiWin",
-    "ifDevOrWinCi",
-    "ifDevOrLinuxCi",
-    "ifWinCi",
-    "ifLinuxOrDevMac",
-  ],
-  function (name, runTest: (context: TestContext) => void | Promise<void>) {
-    const suite = getCurrentSuite()
-
-    const task = suite.task(name)
-    const test = suite.test
-    suite.test = {
-      ...suite.test,
-      mode: this.only ? "only" : this.skip ? "skip" : this.todo ? "todo" : "run",
-      // ifEnv: test.runIf,
-      ifMac: test.runIf(isMac),
-      ifNotMac: test.runIf(!isMac),
-
-      ifWindows: test.runIf(isWindows),
-      ifNotWindows: test.runIf(!isWindows),
-      ifWinCi: test.runIf(isCi && isWindows),
-
-      ifCi: test.runIf(isCi),
-      ifNotCi: test.runIf(!isCi),
-      ifNotCiMac: test.runIf(!isCi && !isMac),
-      ifNotCiWin: test.runIf(!isCi && !isWindows),
-
-      ifDevOrWinCi: test.runIf(!isCi || isWindows),
-      ifDevOrLinuxCi: test.runIf(!isCi || isLinux),
-
-      ifLinux: test.runIf(isLinux),
-      ifLinuxOrDevMac: test.runIf(isLinux || (!isCi && isMac)),
-    } as any
-
-    let alreadyRetried = false
-    const wrapped = async (context: TestContext) => {
-      await Promise.resolve(runTest(context)).catch(error => {
-        alreadyRetried = isSupposedToRetry(error.message ?? error, alreadyRetried)
-        if (alreadyRetried) {
-          console.warn(`Retrying test "${suite.name ? suite.name + "  -  " : ""}${name}" due to flaky error:\n\n${error.message ?? error}`)
-          return new Promise(resolve => setTimeout(resolve, 100)).then(() => wrapped(context))
-        }
-        throw error
-      })
-    }
-    setFn(task, () => wrapped(task.context))
+  let alreadyRetried = false
+  const wrapped = async (context: TestContext) => {
+    await Promise.resolve(runTest(context)).catch(error => {
+      alreadyRetried = isSupposedToRetry(error.message ?? error, alreadyRetried)
+      if (alreadyRetried) {
+        return new Promise(resolve => setTimeout(resolve, 100)).then(() => wrapped(context))
+      }
+      throw error
+    })
   }
-)
 
-export { afterAll, beforeAll, describe } from "vitest"
+  suite.task(name, {
+    ...this, // so "todo"/"skip"/... is tracked correctly
+    handler: wrapped,
+    timeout,
+  })
+}) as CustomTestMatcher
 
-// // this function will be called during collection phase:
-// // don't call function handler here, add it to suite tasks
-// // with "getCurrentSuite().task()" method
-// // note: createTaskCollector provides support for "todo"/"each"/...
-// export const test2 = createTaskCollector(function (name, runTest, timeout) {
-//   const suite = getCurrentSuite()
-
-//   const test: any = suite.test
-
-//   const skip = test.skip
-//   // test.ifEnv = test.runIf
-//   test.ifMac = isMac ? test : skip
-//   test.ifNotMac = isMac ? skip : test
-
-//   test.ifWindows = isWindows ? test : skip
-//   test.ifNotWindows = isWindows ? skip : test
-//   test.ifWinCi = isCi && isWindows ? test : skip
-
-//   test.ifCi = isCi ? test : skip
-//   test.ifNotCi = isCi ? skip : test
-//   test.ifNotCiMac = isCi && isMac ? skip : test
-//   test.ifNotCiWin = isCi && isWindows ? skip : test
-
-//   test.ifDevOrWinCi = !isCi || isWindows ? test : skip
-//   test.ifDevOrLinuxCi = !isCi || isLinux ? test : skip
-
-//   test.ifLinux = isLinux ? test : skip
-//   test.ifLinuxOrDevMac = isLinux || (!isCi && isMac) ? test : skip
-
-//   // createChainable([...testMatchers, ...customTestMatchers], function (this, name, runTest) {
-//   //   const suite = getCurrentSuite()
-//   //   const task = suite.task(name)
-
-//   let alreadyRetried = false
-//   const wrapped = async (context: TestContext) => {
-//     await Promise.resolve(runTest(context)).catch(error => {
-//       alreadyRetried = isSupposedToRetry(error.message ?? error, alreadyRetried)
-//       if (alreadyRetried) {
-//         return new Promise(resolve => setTimeout(resolve, 100)).then(() => wrapped(context))
-//       }
-//       throw error
-//     })
-//   }
-
-//   const config = {
-//     ...this, // so "todo"/"skip"/... is tracked correctly
-//     ifMac: isMac ? test : skip,
-//     ifNotMac: isMac ? skip : test,
-
-//     ifWindows: isWindows ? test : skip,
-//     ifNotWindows: isWindows ? skip : test,
-//     ifWinCi: isCi && isWindows ? test : skip,
-
-//     ifCi: isCi ? test : skip,
-//     ifNotCi: isCi ? skip : test,
-//     ifNotCiMac: isCi && isMac ? skip : test,
-//     ifNotCiWin: isCi && isWindows ? skip : test,
-
-//     ifDevOrWinCi: !isCi || isWindows ? test : skip,
-//     ifDevOrLinuxCi: !isCi || isLinux ? test : skip,
-
-//     ifLinux: isLinux ? test : skip,
-//     ifLinuxOrDevMac: isLinux || (!isCi && isMac) ? test : skip,
-//     meta: {
-//       customPropertyToDifferentiateTask: true,
-//     },
-//     handler: wrapped,
-//     timeout,
-//   }
-//   suite.task(name, config)
-// })
+export { afterAll, beforeAll, describe, vitest } from "vitest"
