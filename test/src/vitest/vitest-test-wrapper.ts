b9aebb004c26a16e276c4912ca82b67bfd2e0c84
Mike Maietta <mmaietta@outlook.com>
2025-03-10 09:36:42 -0700

    add some stupid logging

--- test/src/vitest/vitest-test-wrapper.ts
+++ test/src/vitest/vitest-test-wrapper.ts
@@ -1,5 +1,5 @@
 import { Awaitable, TaskCustomOptions, TestContext } from "vitest"
-import { createTaskCollector, getCurrentSuite } from "vitest/suite"
+import { createChainable, createTaskCollector, getCurrentSuite, setFn } from "vitest/suite"
 import { CustomTestMatcher } from "./vitest"
 
 // Handle electron-builder flaky (due to parallel file operations such as hdiutil and EPERM file locks) tests by retrying
@@ -25,9 +25,12 @@ export const test = createTaskCollector(function (this: TaskCustomOptions, name:
   let alreadyRetried = false
   const wrapped = async (context: TestContext) => {
     await Promise.resolve(runTest(context)).catch(error => {
+      console.error(`Error in test "${suite.name ? suite.name + "  -  " : ""}${name}"`)
+      console.error(JSON.stringify(error))
+      console.error(JSON.stringify({ this: this, name, options, runTest }))
       alreadyRetried = isSupposedToRetry(error.message ?? error, alreadyRetried)
       if (alreadyRetried) {
-        console.warn(`retrying unit test due to flaky error:\n${error.message ?? error}`)
+        console.error(`retrying unit test due to flaky error:\n${error.message ?? error}`)
         return new Promise(resolve => setTimeout(resolve, 500)).then(() => runTest(context))
       }
       throw error
@@ -40,6 +43,70 @@ export const test = createTaskCollector(function (this: TaskCustomOptions, name:
     ...options,
   })
 }) as CustomTestMatcher
+
+// const customTestMatchers = [
+//   "ifEnv",
+//   "ifMac",
+//   "ifNotWindows",
+//   "ifNotMac",
+//   "ifNotCiMac",
+//   "ifWindows",
+//   "ifLinux",
+//   "ifCi",
+//   "ifNotCi",
+//   "ifNotCiWin",
+//   "ifDevOrWinCi",
+//   "ifDevOrLinuxCi",
+//   "ifWinCi",
+//   "ifLinuxOrDevMac",
+// ]
+
+// const testMatchers = ["concurrent", "sequential", "skip", "only", "todo", "fails", "runIf"]
+
+// export const test = createChainable(
+//   [
+//     "concurrent",
+//     "sequential",
+//     "skip",
+//     "only",
+//     "todo",
+//     "fails",
+//     "runIF",
+//     "ifEnv",
+//     "ifMac",
+//     "ifNotWindows",
+//     "ifNotMac",
+//     "ifNotCiMac",
+//     "ifWindows",
+//     "ifLinux",
+//     "ifCi",
+//     "ifNotCi",
+//     "ifNotCiWin",
+//     "ifDevOrWinCi",
+//     "ifDevOrLinuxCi",
+//     "ifWinCi",
+//     "ifLinuxOrDevMac",
+//   ],
+//   function (name: string, runTest: (context: TestContext) => void | Promise<void>) {
+//     const suite = getCurrentSuite()
+
+//     const task = suite.task(name)
+//     const test = suite.test
+
+//     let alreadyRetried = false
+//     const wrapped = async (context: TestContext) => {
+//       await Promise.resolve(runTest(context)).catch(error => {
+//         alreadyRetried = isSupposedToRetry(error.message ?? error, alreadyRetried)
+//         if (alreadyRetried) {
+//           console.warn(`Retrying test "${suite.name ? suite.name + "  -  " : ""}${name}" due to flaky error:\n\n${error.message ?? error}`)
+//           return new Promise(resolve => setTimeout(resolve, 500)).then(() => runTest(context))
+//         }
+//         throw error
+//       })
+//     }
+//     setFn(task, () => wrapped(task.context))
+//   }
+// )
 
 export { test as it }
 export { afterAll, beforeAll, describe, vitest } from "vitest"
