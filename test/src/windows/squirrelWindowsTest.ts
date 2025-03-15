aea79ff568e8bac9c0a9bd9c45cb658e5bd53c79
beyondkmp <beyondkmp@gmail.com>
2025-03-15 19:30:13 +0800

    update ut

--- test/src/windows/squirrelWindowsTest.ts
+++ test/src/windows/squirrelWindowsTest.ts
@@ -1,9 +1,101 @@
-import { Platform } from "electron-builder"
+import { Arch, Platform } from "electron-builder"
 import * as path from "path"
 import { CheckingWinPackager } from "../helpers/CheckingPackager"
-import { assertPack, copyTestAsset } from "../helpers/packTester"
+import { app, assertPack, copyTestAsset } from "../helpers/packTester"
 
 describe("squirrel.windows", { sequential: true }, () => {
+  test.ifNotCiMac("Squirrel.Windows", ({ expect }) =>
+    app(
+      expect,
+      {
+        targets: Platform.WINDOWS.createTarget(["squirrel"]),
+        config: {
+          win: {
+            compression: "normal",
+          },
+          executableName: "test with spaces",
+          electronFuses: {
+            runAsNode: true,
+            enableCookieEncryption: true,
+            enableNodeOptionsEnvironmentVariable: true,
+            enableNodeCliInspectArguments: true,
+            enableEmbeddedAsarIntegrityValidation: true,
+            onlyLoadAppFromAsar: true,
+            loadBrowserProcessSpecificV8Snapshot: true,
+            grantFileProtocolExtraPrivileges: undefined, // unsupported on current electron version in our tests
+          },
+        },
+      },
+      { signedWin: true }
+    )
+  )
+
+  test.ifNotCiMac("artifactName", ({ expect }) =>
+    app(expect, {
+      targets: Platform.WINDOWS.createTarget(["squirrel", "zip"]),
+      config: {
+        win: {
+          // tslint:disable:no-invalid-template-strings
+          artifactName: "Test ${name} foo.${ext}",
+        },
+      },
+    })
+  )
+
+  // very slow
+  test.skip("delta and msi", ({ expect }) =>
+    app(expect, {
+      targets: Platform.WINDOWS.createTarget("squirrel", Arch.ia32),
+      config: {
+        squirrelWindows: {
+          remoteReleases: "https://github.com/develar/__test-app-releases",
+          msi: true,
+        },
+      },
+    }))
+
+  test("squirrel window arm64 msi", ({ expect }) =>
+    app(
+      expect,
+      {
+        targets: Platform.WINDOWS.createTarget("squirrel", Arch.arm64),
+        config: {
+          squirrelWindows: {
+            msi: true,
+          },
+        },
+      },
+      { signedWin: true }
+    ))
+
+  test("squirrel window x64 msi", ({ expect }) =>
+    app(
+      expect,
+      {
+        targets: Platform.WINDOWS.createTarget("squirrel", Arch.x64),
+        config: {
+          squirrelWindows: {
+            msi: true,
+          },
+        },
+      },
+      { signedWin: true }
+    ))
+
+  test("squirrel window ia32 msi", ({ expect }) =>
+    app(
+      expect,
+      {
+        targets: Platform.WINDOWS.createTarget("squirrel", Arch.ia32),
+        config: {
+          squirrelWindows: {
+            msi: true,
+          },
+        },
+      },
+      { signedWin: true }
+    ))
+
   test("detect install-spinner", ({ expect }) => {
     let platformPackager: CheckingWinPackager | null = null
     let loadingGifPath: string | null = null
