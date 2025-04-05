f63296509cdac1e47e7a2acac888f7f085b199f8
beyondkmp <beyondkmp@gmail.com>
2025-04-05 10:52:14 +0800

    prettier

--- packages/app-builder-lib/src/node-module-collector/npmNodeModulesCollector.ts
+++ packages/app-builder-lib/src/node-module-collector/npmNodeModulesCollector.ts
@@ -22,16 +22,16 @@ export class NpmNodeModulesCollector extends NodeModulesCollector<NpmDependency,
 
   protected collectAllDependencies(tree: NpmDependency) {
     for (const [key, value] of Object.entries(tree.dependencies || {})) {
-      const hasNestedDependencies = Object.keys(value.dependencies ?? {}).length > 0;
-      const hasNoDependencies = Object.keys(value._dependencies ?? {}).length === 0;
+      const hasNestedDependencies = Object.keys(value.dependencies ?? {}).length > 0
+      const hasNoDependencies = Object.keys(value._dependencies ?? {}).length === 0
 
       if (hasNestedDependencies) {
-        this.allDependencies.set(`${key}@${value.version}`, value);
-        this.collectAllDependencies(value);
+        this.allDependencies.set(`${key}@${value.version}`, value)
+        this.collectAllDependencies(value)
       }
 
       if (hasNoDependencies) {
-        this.allDependencies.set(`${key}@${value.version}`, value);
+        this.allDependencies.set(`${key}@${value.version}`, value)
       }
     }
   }
@@ -52,7 +52,7 @@ export class NpmNodeModulesCollector extends NodeModulesCollector<NpmDependency,
       deps = this.allDependencies.get(newKey)?.dependencies ?? {}
     }
 
-    this.productionGraph[newKey] = { dependencies: [] };
+    this.productionGraph[newKey] = { dependencies: [] }
     const dependencies = Object.entries(deps)
       .filter(([packageName]) => _deps[packageName])
       .map(([packageName, dependency]) => {
