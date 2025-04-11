1c9af923fdc1a2d8488964f606b0cc361be79fcc
beyondkmp <beyondkmp@gmail.com>
2025-04-11 08:19:06 +0800

    add ut

--- test/src/HoistedNodeModuleTest.ts
+++ test/src/HoistedNodeModuleTest.ts
@@ -120,6 +120,53 @@ test("yarn two package.json", ({ expect }) =>
     }
   ))
 
+test("yarn two package.json without node_modules", ({ expect }) =>
+  assertPack(
+    expect,
+    "test-app-hoisted",
+    {
+      targets: linuxDirTarget,
+    },
+    {
+      isInstallDepsBefore: false,
+      projectDirCreated: async projectDir => {
+        await modifyPackageJson(projectDir, data => {
+          data.dependencies = {
+            "electron-updater": "6",
+            express: "4",
+            "patch-package": "^8.0.0",
+          }
+          data.devDependencies = {
+            electron: "23.2.0",
+            "del-cli": "6",
+            "electron-builder": "26",
+            "fs-extra": "11",
+          }
+          data.build.directories = {
+            app: "app",
+          }
+        })
+
+        // install dependencies in project dir
+        await spawn("npm", ["install"], {
+          cwd: projectDir,
+        })
+
+        mkdirSync(path.join(projectDir, "app"))
+        rmSync(path.join(projectDir, "app", "node_modules"), { recursive: true, force: true })
+        copySync(path.join(projectDir, "index.html"), path.join(projectDir, "app", "index.html"))
+        copySync(path.join(projectDir, "index.js"), path.join(projectDir, "app", "index.js"))
+
+        // delete package.json devDependencies
+        const packageJson = readJsonSync(path.join(projectDir, "package.json"))
+        delete packageJson.devDependencies
+        delete packageJson.build
+        delete packageJson.scripts
+        writeJsonSync(path.join(projectDir, "app", "package.json"), packageJson)
+      },
+      packed: context => verifyAsarFileTree(expect, context.getResources(Platform.LINUX)),
+    }
+  ))
 describe("isInstallDepsBefore=true", { sequential: true }, () => {
   test("yarn workspace for scope name", ({ expect }) =>
     assertPack(
