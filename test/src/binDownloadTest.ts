a2b7e69c7d09572f9492202afa1805b5d045215a
Mike Maietta <mmaietta@outlook.com>
2025-04-23 13:06:01 -0700

    reset process env vars for binDownloadTest

--- test/src/binDownloadTest.ts
+++ test/src/binDownloadTest.ts
@@ -6,24 +6,28 @@ test("download binary from Github", async ({ expect }) => {
 })
 
 test("download binary from Mirror with custom dir", async ({ expect }) => {
+  const previousDir = process.env.ELECTRON_BUILDER_BINARIES_CUSTOM_DIR
+  const previousMirror = process.env.ELECTRON_BUILDER_BINARIES_MIRROR
   process.env.ELECTRON_BUILDER_BINARIES_MIRROR = "https://github.com/electron-userland/electron-builder-binaries/releases/download/"
   process.env.ELECTRON_BUILDER_BINARIES_CUSTOM_DIR = "linux-tools-mac-10.12.3"
   const bin = await getBinFromUrl("linux-tools", "mac-10.12.4", "CMiM/6mWOUghHkvgB2PmJdyGoblMdlGD+VBqbxiIea51ExDDe7GrZ82/wBy3KI0d5Wrrkc1Hkd1/lWMcbWUfuA==")
-  delete process.env.ELECTRON_BUILDER_BINARIES_MIRROR
-  delete process.env.ELECTRON_BUILDER_BINARIES_CUSTOM_DIR
+  process.env.ELECTRON_BUILDER_BINARIES_MIRROR = previousMirror
+  process.env.ELECTRON_BUILDER_BINARIES_CUSTOM_DIR = previousDir
   expect(bin).toBeTruthy()
 })
 
 test("download binary from Mirror", async ({ expect }) => {
+  const previousMirror = process.env.ELECTRON_BUILDER_BINARIES_MIRROR
   process.env.ELECTRON_BUILDER_BINARIES_MIRROR = "https://github.com/electron-userland/electron-builder-binaries/releases/download/"
   const bin = await getBinFromUrl("linux-tools", "mac-10.12.4", "CMiM/6mWOUghHkvgB2PmJdyGoblMdlGD+VBqbxiIea51ExDDe7GrZ82/wBy3KI0d5Wrrkc1Hkd1/lWMcbWUfuA==")
-  delete process.env.ELECTRON_BUILDER_BINARIES_MIRROR
+  process.env.ELECTRON_BUILDER_BINARIES_MIRROR = previousMirror
   expect(bin).toBeTruthy()
 })
 
 test("download binary from Mirror with Url override", async ({ expect }) => {
+  const previousUrl = process.env.ELECTRON_BUILDER_BINARIES_DOWNLOAD_OVERRIDE_URL
   process.env.ELECTRON_BUILDER_BINARIES_DOWNLOAD_OVERRIDE_URL = "https://github.com/electron-userland/electron-builder-binaries/releases/download/linux-tools-mac-10.12.3"
   const bin = await getBinFromUrl("linux-tools", "mac-10.12.4", "CMiM/6mWOUghHkvgB2PmJdyGoblMdlGD+VBqbxiIea51ExDDe7GrZ82/wBy3KI0d5Wrrkc1Hkd1/lWMcbWUfuA==")
-  delete process.env.ELECTRON_BUILDER_BINARIES_DOWNLOAD_OVERRIDE_URL
+  process.env.ELECTRON_BUILDER_BINARIES_DOWNLOAD_OVERRIDE_URL = previousUrl
   expect(bin).toBeTruthy()
 })
