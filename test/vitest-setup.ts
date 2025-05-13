9e5260a9e51928dc1ded5e9eeb9f2779fedcf02a
Mike Maietta <mmaietta@outlook.com>
2025-05-13 07:49:31 -0700

    cleanup

--- test/vitest-setup.ts
+++ test/vitest-setup.ts
@@ -75,6 +75,3 @@ if (!process.env.SZA_COMPRESSION_LEVEL) {
 
 process.env.FORCE_YARN = "true"
 process.env.TEST_SET_BABEL_PRESET = "true"
-
-// process.env.ELECTRON_BUILDER_BINARIES_MIRROR = "https://github.com/mmaietta/electron-builder-binaries/releases/download/"
-// process.env.ELECTRON_BUILDER_BINARIES_CUSTOM_DIR = "v1.0.1"
