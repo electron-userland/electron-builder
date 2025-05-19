1d15045b1d536567f1a855ba4e701a3486a51948
Mike Maietta <mmaietta@outlook.com>
2025-05-19 09:02:03 -0700

    update snapshots

--- packages/app-builder-lib/src/util/config/validate.ts
+++ packages/app-builder-lib/src/util/config/validate.ts
@@ -9,11 +9,41 @@ import { Nullish } from "builder-util-runtime"
 import { PACKAGE_VERSION } from "../../version.js"
 import { Configuration } from "../../configuration.js"
 import { readJson } from "fs-extra"
+import validateSchema from "@develar/schema-utils"
 
-export const validateConfiguration = async (config: Configuration, debugLogger: DebugLogger): Promise<void> => {
-  const validator = new ConfigurationValidator()
-  await validator.validate(config, debugLogger)
+const schemeDataPromise = new Lazy(() => readJson(path.join(__dirname, "..", "..", "..", "scheme.json")))
+
+export async function validateConfiguration(config: Configuration, debugLogger: DebugLogger) {
+  validateSchema(await schemeDataPromise.value, config, {
+    name: `electron-builder ${PACKAGE_VERSION}`,
+    postFormatter: (formattedError: string, error: any): string => {
+      if (debugLogger.isEnabled) {
+        debugLogger.add("invalidConfig", safeStringifyJson(error))
+      }
+
+      const site = "https://www.electron.build"
+      let url = `${site}/configuration`
+      const targets = new Set(["mac", "dmg", "pkg", "mas", "win", "nsis", "appx", "linux", "appimage", "snap"])
+      const dataPath: string = error.dataPath == null ? null : error.dataPath
+      const targetPath = dataPath.startsWith(".") ? dataPath.substring(1).toLowerCase() : null
+      if (targetPath != null && targets.has(targetPath)) {
+        url = `${site}/${targetPath}`
+      }
+
+      return `${formattedError}\n  How to fix:
+  1. Open ${url}
+  2. Search the option name on the page (or type in into Search to find across the docs).
+    * Not found? The option may have been deprecated or no longer exists (check spelling).
+    * Found? Check that the option in the appropriate place. e.g. "title" only in the "dmg", not in the root.
+`
+    },
+  })
 }
+
+// export const validateConfiguration = async (config: Configuration, debugLogger: DebugLogger): Promise<void> => {
+//   const validator = new ConfigurationValidator()
+//   await validator.validate(config, debugLogger)
+// }
 
 class ConfigurationValidator {
   private readonly pluginName = "electron-builder"
