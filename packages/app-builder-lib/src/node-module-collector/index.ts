0027db22992d5dd1bff29782a6cef93f9f18b641
Mike Maietta <mmaietta@outlook.com>
2025-06-01 17:05:18 -0700

    ok

--- packages/app-builder-lib/src/node-module-collector/index.ts
+++ packages/app-builder-lib/src/node-module-collector/index.ts
@@ -7,7 +7,7 @@ import { exec } from "builder-util"
 
 async function isPnpmProjectHoisted(rootDir: string) {
   const command = getPackageManagerCommand(PM.PNPM)
-  const config = await exec(command, ["config", "list"], { cwd: rootDir })
+  const config = await exec(command, ["config", "list"], { cwd: rootDir, shell: true })
   const lines = Object.fromEntries(config.split("\n").map(line => line.split("=").map(s => s.trim())))
   return lines["node-linker"] === "hoisted"
 }
