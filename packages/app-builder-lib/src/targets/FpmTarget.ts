ecfffcb4dff4b75688c7e1e121aa3de671d1e3d1
Mike Maietta <mmaietta@outlook.com>
2025-07-01 07:49:13 -0700

    cleanup

--- packages/app-builder-lib/src/targets/FpmTarget.ts
+++ packages/app-builder-lib/src/targets/FpmTarget.ts
@@ -253,9 +253,6 @@ export default class FpmTarget extends Target {
 
     const env = {
       ...process.env,
-      // SZA_PATH: await getPath7za(),
-      // SZA_COMPRESSION_LEVEL: packager.compression === "store" ? "0" : "9",
-      // SZA_ARCHIVE_TYPE: "xz",
     }
 
     // rpmbuild wants directory rpm with some default config files. Even if we can use dylibbundler, path to such config files are not changed (we need to replace in the binary)
