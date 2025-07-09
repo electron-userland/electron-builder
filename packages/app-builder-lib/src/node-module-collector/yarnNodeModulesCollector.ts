fa708a9124e7a65c71951da156b53b299dad17e5
beyondkmp <beyondkmp@gmail.com>
2025-07-09 15:32:07 +0800

    update comment

--- packages/app-builder-lib/src/node-module-collector/yarnNodeModulesCollector.ts
+++ packages/app-builder-lib/src/node-module-collector/yarnNodeModulesCollector.ts
@@ -7,6 +7,6 @@ export class YarnNodeModulesCollector extends NpmNodeModulesCollector {
   }
 
   // note: do not override instance-var `pmCommand`. We explicitly use npm for the json payload
-  // yarn list cannot get the detailed dependencies path info, so we use npm to get the json payload
+  // yarn list cannot get the detailed dependencies path info, But we can use npm to get all dependencies path info
   public readonly installOptions = { manager: PM.NPM, lockfile: "yarn.lock" }
 }
