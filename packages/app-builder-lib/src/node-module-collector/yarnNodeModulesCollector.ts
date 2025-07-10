35f5f6e55762ffc377fcd5587a8cea8753184d50
Mike Maietta <mmaietta@users.noreply.github.com>
2025-07-10 16:54:41 -0700

    fix: Use `spawn` -> `shell: false` for node module collection (#9177)

--- packages/app-builder-lib/src/node-module-collector/yarnNodeModulesCollector.ts
+++ packages/app-builder-lib/src/node-module-collector/yarnNodeModulesCollector.ts
@@ -2,10 +2,6 @@ import { NpmNodeModulesCollector } from "./npmNodeModulesCollector"
 import { PM } from "./packageManager"
 
 export class YarnNodeModulesCollector extends NpmNodeModulesCollector {
-  constructor(rootDir: string) {
-    super(rootDir)
-  }
-
   // note: do not override instance-var `pmCommand`. We explicitly use npm for the json payload
   // yarn list cannot get the detailed dependencies path info, But we can use npm to get all dependencies path info
   public readonly installOptions = { manager: PM.NPM, lockfile: "yarn.lock" }
