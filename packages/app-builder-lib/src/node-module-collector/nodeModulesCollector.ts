349cd5e691d6056de929dc4a4c970f644b027c6b
beyondkmp <beyondkmp@gmail.com>
2025-09-12 17:24:02 +0800

    prettier

--- packages/app-builder-lib/src/node-module-collector/nodeModulesCollector.ts
+++ packages/app-builder-lib/src/node-module-collector/nodeModulesCollector.ts
@@ -140,14 +140,14 @@ export abstract class NodeModulesCollector<T extends Dependency<T, OptionalsType
     for (const d of dependencies.values()) {
       const reference = [...d.references][0]
       const p = this.allDependencies.get(`${d.name}@${reference}`)?.path
-      if (p === undefined ) {
+      if (p === undefined) {
         log.debug({ name: d.name, reference }, "cannot find path for dependency")
         continue
       }
 
       // fix npm list issue
       // https://github.com/npm/cli/issues/8535
-      if(!fs.existsSync(p)) {
+      if (!fs.existsSync(p)) {
         log.debug({ name: d.name, reference, p }, "dependency path does not exist")
         continue
       }
