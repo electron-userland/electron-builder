5551da368456f9e7caff54066fb184f23a7378ed
Mike Maietta <mmaietta@outlook.com>
2025-12-18 08:30:00 -0800

    force traversal for Bun

--- packages/app-builder-lib/src/node-module-collector/bunNodeModulesCollector.ts
+++ packages/app-builder-lib/src/node-module-collector/bunNodeModulesCollector.ts
@@ -1,13 +1,13 @@
 import { log } from "builder-util"
 import { PM } from "./packageManager"
-import { NpmDependency } from "./types"
 import { TraversalNodeModulesCollector } from "./traversalNodeModulesCollector"
+import { TraversedDependency } from "./types"
 
 export class BunNodeModulesCollector extends TraversalNodeModulesCollector {
   public readonly installOptions = { manager: PM.BUN, lockfile: "bun.lock" }
 
-  protected async getDependenciesTree(_pm: PM): Promise<NpmDependency> {
-    log.info(null, "note: bun does not support any CLI for dependency tree extraction, utilizing NPM node module collector instead")
-    return super.getDependenciesTree(PM.NPM)
+  protected async getDependenciesTree(_pm: PM): Promise<TraversedDependency> {
+    log.info(null, "note: bun does not support any CLI for dependency tree extraction, utilizing manual traversal collector instead")
+    return super.getDependenciesTree(PM.TRAVERSAL)
   }
 }
