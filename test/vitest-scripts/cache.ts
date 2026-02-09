4a99e8b1dc55bb96c0e44649d18b169d76e37d65
Mike Maietta <mmaietta@users.noreply.github.com>
2026-02-08 20:31:22 -0800

    chore: change `vitest` sharding approach to be platform-specific time estimations (#9576)

--- test/vitest-scripts/cache.ts
+++ test/vitest-scripts/cache.ts
@@ -3,21 +3,28 @@ import * as path from "path"
 import { CACHE_FILE, SupportedPlatforms } from "./smart-config"
 
 export interface TestStats {
-  runs: number
-  fails: number
-  avgMs: number
-  slow?: boolean
+  platformRuns?: Record<
+    SupportedPlatforms,
+    {
+      runs: number
+      fails: number
+      avgMs: number
+    }
+  >
   heavy?: boolean
 }
 
 export interface FileStats {
-  runs: number
-  fails: number
-  avgMs: number
   unstable?: boolean
   hasHeavyTests?: boolean
-  platformAvgMs?: Record<SupportedPlatforms, number>
-  platformRuns?: Record<SupportedPlatforms, number>
+  platformRuns?: Record<
+    SupportedPlatforms,
+    {
+      runs: number
+      fails: number
+      avgMs: number
+    }
+  >
 }
 
 interface SmartCache {
