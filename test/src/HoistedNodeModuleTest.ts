f93210c1315df9cb9794aa6e5c6b08a8b4c8e62c
Mike Maietta <mmaietta@users.noreply.github.com>
2026-04-21 19:40:07 -0700

    chore: updating snapshots and allowing docker-build to be triggered manually from GHA (#9686)

--- test/src/HoistedNodeModuleTest.ts
+++ test/src/HoistedNodeModuleTest.ts
@@ -98,8 +98,7 @@ describe.ifNotWindows("node_module collectors", () => {
         projectDirCreated: async (projectDir, _tmpDir, testEnv) => {
           await modifyPackageJson(projectDir, data => {
             data.dependencies = {
-              "electron-updater": "6.8.3",
-              express: "4.0.0",
+              "is-odd": "3.0.1",
             }
             data.devDependencies = {
               electron: ELECTRON_VERSION,
@@ -150,7 +149,7 @@ describe.ifNotWindows("node_module collectors", () => {
         projectDirCreated: async (projectDir, _tmpDir, testEnv) => {
           await modifyPackageJson(projectDir, data => {
             data.dependencies = {
-              "electron-updater": "6.8.3",
+              "is-odd": "3.0.1",
             }
             data.devDependencies = {
               electron: ELECTRON_VERSION,
@@ -472,7 +471,7 @@ describe.ifNotWindows("node_module collectors", () => {
               "electron-clear-data": "1.0.5",
             }
             data.devDependencies = {
-              electron: "34.0.2",
+              electron: ELECTRON_VERSION,
             }
           })
           await spawn("yarn", ["install"], {
