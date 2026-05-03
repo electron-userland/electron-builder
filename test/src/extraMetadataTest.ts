c66e42b4bf43d617b0a0dd9a8672b0d22e5680e6
Mike Maietta <mmaietta@outlook.com>
2026-05-02 20:41:18 -0700

    fix import

--- test/src/extraMetadataTest.ts
+++ test/src/extraMetadataTest.ts
@@ -70,7 +70,7 @@ test("cli", ({ expect }) => {
   const yargs = createYargs()
 =======
   // because these methods are internal
-  const { configureBuildCommand, normalizeOptions } = require("electron-builder")
+  const { configureBuildCommand, normalizeOptions } = require("electron-builder/internal")
   const yargs = require("yargs")
 >>>>>>> fb7cff668 (esm complete on tests as well?)
     .strict()
