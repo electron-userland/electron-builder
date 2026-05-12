84aca33ff9c6c47ee5ea9f148b4fedd859420193
Mike Maietta <mmaietta@outlook.com>
2026-05-12 14:09:33 -0700

    chore(docker): Updating docker images being deployed for node. Adding 24.15.0 node base image

--- docker/build.sh
+++ docker/build.sh
@@ -6,9 +6,10 @@ NODE_VERSION=$1
 NODE_TAG=$(cut -d '.' -f 1 <<< "$NODE_VERSION")
 DATE=$(date +%m.%y)
 
-docker build -t electronuserland/builder:base -t "electronuserland/builder:base-$DATE" docker/base
+docker build --provenance=false -t electronuserland/builder:base -t "electronuserland/builder:base-$DATE" docker/base
 
 docker build \
+  --provenance=false \
   --build-arg NODE_VERSION="$NODE_VERSION" \
   --build-arg IMAGE_VERSION="base-$DATE" \
   -t "electronuserland/builder:$NODE_TAG" \
@@ -16,12 +17,14 @@ docker build \
   docker/node
 
 docker build \
+  --provenance=false \
   --build-arg IMAGE_VERSION="$NODE_TAG-$DATE" \
   -t "electronuserland/builder:$NODE_TAG-wine" \
   -t "electronuserland/builder:$NODE_TAG-wine-$DATE" \
   docker/wine
 
 docker build \
+  --provenance=false \
   --build-arg IMAGE_VERSION="$NODE_TAG-wine-$DATE" \
   -t "electronuserland/builder:$NODE_TAG-wine-chrome" \
   -t "electronuserland/builder:$NODE_TAG-wine-chrome-$DATE" \
@@ -29,6 +32,7 @@ docker build \
 
 # We also use this image for running our unit tests
 docker build \
+  --provenance=false \
   --build-arg IMAGE_VERSION="$NODE_TAG-wine-$DATE" \
   -t "electronuserland/builder:$NODE_TAG-wine-mono" \
   -t "electronuserland/builder:$NODE_TAG-wine-mono-$DATE" \
