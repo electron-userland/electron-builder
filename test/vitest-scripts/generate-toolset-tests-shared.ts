a2e523ac1074c80caaf30acf1f31048e3f374181
Mike Maietta <mmaietta@outlook.com>
2026-05-23 19:39:48 -0700

    use fs-extra for cleanup?

--- test/vitest-scripts/generate-toolset-tests-shared.ts
+++ test/vitest-scripts/generate-toolset-tests-shared.ts
@@ -1,4 +1,4 @@
-import * as fs from "fs"
+import * as fs from "fs-extra"
 import * as path from "path"
 import type { TestOptions } from "vitest"
 import type { ConditionalChainProps } from "../typings/vitest"
