11e6ccc6e75c1ff3de6c9e92d7f294214074bbda
Mike Maietta <mmaietta@users.noreply.github.com>
2026-06-02 06:58:18 -0700

    rename test folder due to weird windowsOS overlap

---
+++ test/vitest-scripts/generate-tests.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env ts-node
+import { existsSync, rmSync } from "fs-extra"
+import { generateLinuxToolsetTests } from "./runtime-tests/generate-toolset-tests-linux"
+import { generateMacToolsetTests } from "./runtime-tests/generate-toolset-tests-mac"
+import { GENERATED_TESTS_DIR } from "./runtime-tests/generate-toolset-tests-shared"
+import { generateWindowsToolsetTests } from "./runtime-tests/generate-toolset-tests-windows"
+
+const generateTests = () => {
+  if (existsSync(GENERATED_TESTS_DIR)) {
+    // Clear out old generated tests before generating new ones
+    rmSync(GENERATED_TESTS_DIR, { recursive: true })
+  }
+  generateLinuxToolsetTests()
+  generateMacToolsetTests()
+  generateWindowsToolsetTests()
+}
+
+if (require.main === module) {
+  generateTests()
+}
+
+export { generateTests }
