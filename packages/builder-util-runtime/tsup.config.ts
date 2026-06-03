6a69e20852c537ca04bd6af6c041e72b6c8f1525
Mike Maietta <mmaietta@outlook.com>
2026-06-03 08:28:30 -0700

    update engines to 22.12

--- packages/builder-util-runtime/tsup.config.ts
+++ packages/builder-util-runtime/tsup.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from "tsup"
+import { baseConfig } from "../../tsup.config.base"
 
 export default defineConfig({
+  ...baseConfig,
   entry: {
     index: "src/index.ts",
     indexInternal: "src/indexInternal.ts",
   },
-  format: ["cjs", "esm"],
-  dts: true,
-  tsconfig: "../tsconfig-tsup.json",
-  sourcemap: true,
-  clean: true,
-  outDir: "dist",
-  platform: "node",
 })
--- packages/electron-builder/tsup.config.ts
+++ packages/electron-builder/tsup.config.ts
@@ -1,23 +1,19 @@
 import { defineConfig } from "tsup"
+import { baseConfig } from "../../tsup.config.base"
 
 export default defineConfig({
+  ...baseConfig,
   entry: {
     index: "src/index.ts",
     indexInternal: "src/indexInternal.ts",
     "cli/cli": "src/cli/cli.ts",
     "cli/install-app-deps": "src/cli/install-app-deps.ts",
   },
-  format: ["cjs", "esm"],
   dts: {
     entry: {
       index: "src/index.ts",
       indexInternal: "src/indexInternal.ts",
     },
   },
-  tsconfig: "../tsconfig-tsup.json",
-  sourcemap: true,
-  clean: true,
-  outDir: "dist",
-  platform: "node",
   shims: true,
 })
