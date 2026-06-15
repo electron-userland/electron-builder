005a8455da6b4ecfabceef5d25b3decdb4620fe4
Mike Maietta <mmaietta@outlook.com>
2026-06-15 12:17:12 -0700

    temporarily disable flaky mac update test

--- test/src/updater/blackboxUpdateMacSuite.ts
+++ test/src/updater/blackboxUpdateMacSuite.ts
@@ -3,14 +3,16 @@ import { TestContext } from "vitest"
 import { runTest } from "./blackboxUpdateHelpers"
 
 export function registerBlackboxMacTests(): void {
-  test("x64", async (context: TestContext) => {
-    await runTest(context, "zip", "", Arch.x64)
-  })
-  test("universal", async (context: TestContext) => {
-    await runTest(context, "zip", "", Arch.universal)
-  })
-  // only will update on arm64 mac
-  test.ifEnv(process.arch === "arm64")("arm64", async (context: TestContext) => {
-    await runTest(context, "zip", "", Arch.arm64)
+  describe.skip("mac auto-update", () => {
+    test("x64", async (context: TestContext) => {
+      await runTest(context, "zip", "", Arch.x64)
+    })
+    test("universal", async (context: TestContext) => {
+      await runTest(context, "zip", "", Arch.universal)
+    })
+    // only will update on arm64 mac
+    test.ifEnv(process.arch === "arm64")("arm64", async (context: TestContext) => {
+      await runTest(context, "zip", "", Arch.arm64)
+    })
   })
 }
